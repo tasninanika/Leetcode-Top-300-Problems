4b040369f2395a15d4867128f451be2120e9e5e4
anikatasnin <jarintasnin09@gmail.com>
2025-04-12 20:57:25 +0600

    Add Remove Duplicates from Sorted Array

--- Arrays/Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.cpp
+++ Arrays/Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.cpp
@@ -3,6 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         for(int i = 0; i < nums.size() - 1; i++){
             if(nums[i] == nums[i+1]){
+                nums.pop_back(nums[i+1]);
 
             }
         }
