7ac10618ed6345cc418dcbc94eea92db2c85ced4
anikatasnin <jarintasnin09@gmail.com>
2025-04-15 11:32:14 +0600

    Add Best Time to Buy and Sell Stock II

--- Arrays/Best Time to Buy and Sell Stock II/BestTimeToBuyAndSellStockII.cpp
+++ Arrays/Best Time to Buy and Sell Stock II/BestTimeToBuyAndSellStockII.cpp
@@ -4,6 +4,7 @@ public:
         int maxProfit = 0;
         for (int i = 1; i < prices.size(); i++) {
             if (prices[i] > prices[i - 1]) {
+                maxProfit += (prices[i] - prices[i - 1]);
 
     }
 };
