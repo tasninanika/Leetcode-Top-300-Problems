85707a957f6e01a27775174e95d9b188d41d77a5
anikatasnin <jarintasnin09@gmail.com>
2025-04-16 23:19:05 +0600

    Add Number of Zero-Filled Subarrays

--- Arrays/Number of Zero-Filled Subarrays/NumberOfZero-FilledSubarrays.cpp
+++ Arrays/Number of Zero-Filled Subarrays/NumberOfZero-FilledSubarrays.cpp
@@ -3,5 +3,9 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long c = 0;
         long long result = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+
+        }
     }
 };
