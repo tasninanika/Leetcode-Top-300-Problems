5103dbe490bf6f9165bfb6eb633d0d42aa107c80
anikatasnin <jarintasnin09@gmail.com>
2025-04-18 00:08:10 +0600

    Add Increasing Triplet Subsequence

--- Arrays/Increasing Triplet Subsequence/IncreasingTripletSubsequence.cpp
+++ Arrays/Increasing Triplet Subsequence/IncreasingTripletSubsequence.cpp
@@ -4,6 +4,8 @@ public:
         int first = INT_MAX, second = INT_MAX;
         int c = 0;
         for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] <= first) {
+                first = nums[i];
 
     }
 };
