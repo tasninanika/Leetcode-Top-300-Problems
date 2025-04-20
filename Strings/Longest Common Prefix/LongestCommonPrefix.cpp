af7ce8f8709a4142290cc7613472814197e77cbe
anikatasnin <jarintasnin09@gmail.com>
2025-04-20 09:40:39 +0600

    Add Longest Common Prefix

--- Strings/Longest Common Prefix/LongestCommonPrefix.cpp
+++ Strings/Longest Common Prefix/LongestCommonPrefix.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-         if (strs.empty()) {
+        if (strs.empty()) {
             return "";
         }
         string macthing_word = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(macthing_word) != 0) {
+                macthing_word.pop_back();
+            }
+        }
 
     }
 };
