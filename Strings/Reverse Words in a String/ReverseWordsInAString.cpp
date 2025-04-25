103f10317282d073c6e02393e8fe8afa82201450
anikatasnin <jarintasnin09@gmail.com>
2025-04-25 12:31:50 +0600

    Add Reverse Words in a String

--- Strings/Reverse Words in a String/ReverseWordsInAString.cpp
+++ Strings/Reverse Words in a String/ReverseWordsInAString.cpp
@@ -12,7 +12,10 @@ public:
             if(i >= n){
                 break;
             }
-            string word;
+            string word = " ";
+            while(i < n && s[i] != ' '){
+
+            }
         }
     }
 };
