aa7a432ad24eaab6f8416c8689f9c08de76e8f70
anikatasnin <jarintasnin09@gmail.com>
2025-04-26 09:12:19 +0600

    Add Guess the Word

--- Strings/Guess the Word/GuessTheWord.cpp
+++ Strings/Guess the Word/GuessTheWord.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     void findSecretWord(vector<string>& words, Master& master) {
-        int solve(string s1, string s2) {
+        int solve(string s1, string s2){
             int ans = 0;
-            for (int i = 0; i < s1.size(); i++) {
+            for (int i = 0; i < s1.size(); i++){
+                if (s1[i] == s2[i])
+
 
 
     }
