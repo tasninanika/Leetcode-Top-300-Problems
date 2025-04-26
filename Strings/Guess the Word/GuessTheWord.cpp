fcb6c2f51812baf6d9ca6bfae649b3b90ed5f693
anikatasnin <jarintasnin09@gmail.com>
2025-04-26 09:12:50 +0600

    Add Guess the Word

--- Strings/Guess the Word/GuessTheWord.cpp
+++ Strings/Guess the Word/GuessTheWord.cpp
@@ -4,7 +4,9 @@ public:
         int solve(string s1, string s2){
             int ans = 0;
             for (int i = 0; i < s1.size(); i++){
-                if (s1[i] == s2[i])
+                if (s1[i] == s2[i]){
+                    ans++;
+                }
 
 
 
