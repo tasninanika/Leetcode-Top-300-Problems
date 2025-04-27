52fc87fade0bbe8b21b86109bad1536d8163174c
anikatasnin <jarintasnin09@gmail.com>
2025-04-27 09:03:03 +0600

    Add Single Number

--- Bit Manipulation/Single Number/SingleNumber.cpp
+++ Bit Manipulation/Single Number/SingleNumber.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int single_value = 0;
         for(int i = 0; i < nums.size(); i++){
-
+            single_value ^= nums[i];
         }
 
     }
