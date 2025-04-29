d2b123c4c77b21d7d7c4b4a65d90ee449366fc56
anikatasnin <jarintasnin09@gmail.com>
2025-04-29 09:25:16 +0600

    Add Counting Bits

--- Bit Manipulation/Counting Bits/CountingBits.cpp
+++ Bit Manipulation/Counting Bits/CountingBits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1, 0);
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
