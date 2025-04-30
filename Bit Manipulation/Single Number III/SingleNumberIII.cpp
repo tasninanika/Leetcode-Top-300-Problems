7801f98f35de0746e3802b1d72794c7a5fc6d7f4
anikatasnin <jarintasnin09@gmail.com>
2025-04-30 09:24:13 +0600

    Add Single Number III

--- Bit Manipulation/Single Number III/SingleNumberIII.cpp
+++ Bit Manipulation/Single Number III/SingleNumberIII.cpp
@@ -2,5 +2,10 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         unordered_map<int, int> frequency;
+
+        for (int i = 0; i < nums.size(); i++) {
+            frequency[nums[i]]++;
+        }
+
     }
 };
