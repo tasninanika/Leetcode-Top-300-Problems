077cf8f3ecfbec39a51fd6568e59e367ad30ca1d
anikatasnin <jarintasnin09@gmail.com>
2025-06-22 11:53:39 +0600

    Add Contains Duplicate II

---
+++ Hash Tables/Contains Duplicate II/ContainsDuplicateII.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> numIndexMap;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (numIndexMap.find(nums[i]) != numIndexMap.end() && i - numIndexMap[nums[i]] <= k) {
+                return true;
+            }
+            numIndexMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
