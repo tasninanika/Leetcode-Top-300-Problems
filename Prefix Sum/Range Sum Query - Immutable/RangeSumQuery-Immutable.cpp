dbba46ad7c45136afc4cb46e97adced1e2f0204c
anikatasnin <jarintasnin09@gmail.com>
2025-06-24 09:16:40 +0600

    Add Range Sum Query - Immutable

--- Prefix Sum/Range Sum Query - Immutable/RangeSumQuery-Immutable.cpp
+++ Prefix Sum/Range Sum Query - Immutable/RangeSumQuery-Immutable.cpp
@@ -1,1 +1,18 @@
+class NumArray {
+public:
+    NumArray(vector<int>& nums){
+        int n = nums.size();
+        prefixSum.resize(n + 1, 0);
 
+        for(int i = 0; i < n; ++i){
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+
+    private:
+    vector<int> prefixSum;
+};
