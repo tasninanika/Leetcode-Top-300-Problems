34cd80020764149d5f03bfa1c416b6f5e2b47794
anikatasnin <jarintasnin09@gmail.com>
2025-06-24 11:13:16 +0600

    Add Max Consecutive Ones III

---
+++ Sliding Window - Dynamic Size/Max Consecutive Ones III/MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, right = 0;
+        int zeroCount = 0;
+        int maxLength = 0;
+
+        while(right < nums.size()){
+            if(nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while(zeroCount > k){
+                if(nums[left] == 0){
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLength = max(maxLength, right - left + 1);
+
+            right++;
+        }
+
+        return maxLength;
+    }
+};
