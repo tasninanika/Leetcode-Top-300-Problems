769c431a093dca208301078552ce69f4e14a576a
anikatasnin <jarintasnin09@gmail.com>
2025-06-25 10:13:32 +0600

    Add Valid Sudoku

---
+++ Matrix (2D Array)/Valid Sudoku/ValidSudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rows(9), columns(9), boxes(9);
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char num = board[i][j];
+                if(num != '.'){
+                    int boxIndex = (i / 3) * 3 + j / 3;
+
+                    if(rows[i].count(num) || columns[j].count(num) || boxes[boxIndex].count(num)) {
+                        return false;
+                    }
+
+                    rows[i].insert(num);
+                    columns[j].insert(num);
+                    boxes[boxIndex].insert(num);
+                }
+            }
+        }
+
+        return true;
+    }
+};
