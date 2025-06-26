8faa5e7ee29d1051cb85687c5248ec9a5a506131
anikatasnin <jarintasnin09@gmail.com>
2025-06-27 00:44:32 +0600

    Add Remove Duplicates from Sorted List II

--- Linked List/Remove Duplicates from Sorted List II/RemoveDuplicatesFromSortedListII.cpp
+++ Linked List/Remove Duplicates from Sorted List II/RemoveDuplicatesFromSortedListII.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
 
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* prev = dummy;
+
+        while (head) {
+            if (head->next && head->val == head->next->val) {
+                while (head->next && head->val == head->next->val) {
+                    head = head->next;
+                }
+                prev->next = head->next;
+            }
+            else {
+                prev = prev->next;
+            }
+            head = head->next;
+        }
+
+        return dummy->next;
+    }
+};
