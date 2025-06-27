5f9a832aca01269a39c1a564bc7a2e867826631a
anikatasnin <jarintasnin09@gmail.com>
2025-06-27 10:21:52 +0600

    Add Flatten a Multilevel Doubly Linked List

--- Linked List/Flatten a Multilevel Doubly Linked List/FlattenaMultilevelDoublyLinkedList.cpp
+++ Linked List/Flatten a Multilevel Doubly Linked List/FlattenaMultilevelDoublyLinkedList.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(!head)
+            return nullptr;
 
+        stack<Node*> stack;
+        stack.push(head);
+
+        Node* prev = nullptr;
+
+        while(!stack.empty()){
+            Node* current = stack.top();
+            stack.pop();
+
+            if(prev){
+                prev->next = current;
+                current->prev = prev;
+            }
+
+            if(current->next){
+                stack.push(current->next);
+            }
+
+            if(current->child){
+                stack.push(current->child);
+                current->child = nullptr;
+            }
+
+            prev = current;
+        }
+
+        return head;
+    }
+};
