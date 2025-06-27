16de0e54eee83228a8b89e6fa2ad370dd3628f5d
anikatasnin <jarintasnin09@gmail.com>
2025-06-27 10:14:43 +0600

    Add Rotate List

--- Linked List/Rotate List/RotateList.cpp
+++ Linked List/Rotate List/RotateList.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k == 0)
+            return head;
 
+        int length = 1;
+        ListNode* lastNode = head;
+
+        while(lastNode->next){
+            lastNode = lastNode->next;
+            length++;
+        }
+
+        lastNode->next = head;
+
+        int stepsToNewHead = length - k % length;
+        ListNode* newTail = head;
+
+        for(int i = 1; i < stepsToNewHead; i++){
+            newTail = newTail->next;
+        }
+
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
+    }
+};
