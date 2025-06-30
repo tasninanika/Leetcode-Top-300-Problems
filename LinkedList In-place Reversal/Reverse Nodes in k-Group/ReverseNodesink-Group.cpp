ebfec1aba582b257e213e1785b885e9369b0881e
anikatasnin <jarintasnin09@gmail.com>
2025-06-30 23:05:42 +0600

    Add Reverse Nodes in k-Group

--- LinkedList In-place Reversal/Reverse Nodes in k-Group/ReverseNodesink-Group.cpp
+++ LinkedList In-place Reversal/Reverse Nodes in k-Group/ReverseNodesink-Group.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* curr = head;
+        int count = 0;
+        while(curr != nullptr && count < k){
+            curr = curr->next;
+            count++;
+        }
 
+        if(count == k){
+            ListNode* reversedHead = reverseKGroup(curr, k);
+
+            while(count-- > 0){
+                ListNode* temp = head->next;
+                head->next = reversedHead;
+                reversedHead = head;
+                head = temp;
+            }
+            head = reversedHead;
+        }
+        return head;
+    }
+};
