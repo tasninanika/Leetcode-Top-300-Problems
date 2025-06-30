4b6b705bcd69820e703e90da3197a56e2ccb3f5b
anikatasnin <jarintasnin09@gmail.com>
2025-06-30 23:02:12 +0600

    Add Reverse Linked List II

--- LinkedList In-place Reversal/Reverse Linked List II/ReverseLinkedListII.cpp
+++ LinkedList In-place Reversal/Reverse Linked List II/ReverseLinkedListII.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head || left == right){
+            return head;
+        }
 
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        for(int i = 1; i < left; ++i){
+            prev = prev->next;
+        }
+
+        ListNode* current = prev->next;
+        ListNode* nextNode = current->next;
+
+        for(int i = 0; i < right - left; i++){
+            current->next = nextNode->next;
+            nextNode->next = prev->next;
+            prev->next = nextNode;
+            nextNode = current->next;
+        }
+
+        return dummy->next;
+    }
+};
