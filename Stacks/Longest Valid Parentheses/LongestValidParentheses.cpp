1dbbc35b0175fcc8f1ca8f89547d9ecaf88b8e17
anikatasnin <jarintasnin09@gmail.com>
2025-07-03 19:19:53 +0600

    Add Longest Valid Parentheses

---
+++ Stacks/Longest Valid Parentheses/LongestValidParentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, max_len = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left == right) {
+                max_len = max(max_len, 2 * right);
+            }
+            else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s[i] == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left == right) {
+                max_len = max(max_len, 2 * left);
+            }
+            else if (left > right) {
+                left = right = 0;
+            }
+        }
+
+        return max_len;
+    }
+};
