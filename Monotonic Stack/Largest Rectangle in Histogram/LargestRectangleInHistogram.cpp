952562b9aaf776a0a7b6e95c7e85f16405a206ba
anikatasnin <jarintasnin09@gmail.com>
2025-07-04 10:48:43 +0600

    Add Largest Rectangle in Histogram

--- Monotonic Stack/Largest Rectangle in Histogram/LargestRectangleInHistogram.cpp
+++ Monotonic Stack/Largest Rectangle in Histogram/LargestRectangleInHistogram.cpp
@@ -1,20 +1,20 @@
-int largestRectangleAreaStack(vector<int>& heights) {
-    stack<int> st;
-    int maxArea = 0;
-    int n = heights.size();
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
 
-    for (int i = 0; i <= n; i++) {
-        while (!st.empty() && (i == n || heights[st.top()] > heights[i])) {
-            int height = heights[st.top()];
-            st.pop();
-            int width;
-            if (st.empty()) width = i;
-            else width = i - st.top() - 1;
-            int area = height * width;
-            if (area > maxArea) maxArea = area;
+        for (int i = 0; i <= n; ++i) {
+            while (!st.empty() && (i == n || heights[i] < heights[st.top()])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, height * width);
+            }
+            st.push(i);
         }
-        st.push(i);
-    }
 
-    return maxArea;
-}
+        return maxArea;
+    }
+};
