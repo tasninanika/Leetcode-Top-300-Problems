321786d990241b2bd0991f6312142c3cb15a4607
anikatasnin <jarintasnin09@gmail.com>
2025-07-04 10:34:04 +0600

    Add Next Greater Element I

--- Monotonic Stack/Next Greater Element I/NextGreaterElementI.cpp
+++ Monotonic Stack/Next Greater Element I/NextGreaterElementI.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nge_map;
+        stack<int> s;
+
+        for (int num : nums2) {
+            while (!s.empty() && num > s.top()) {
+                nge_map[s.top()] = num;
+                s.pop();
+            }
+            s.push(num);
+        }
+
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(nge_map.count(num) ? nge_map[num] : -1);
+        }
+
+        return result;
+    }
+};
 
