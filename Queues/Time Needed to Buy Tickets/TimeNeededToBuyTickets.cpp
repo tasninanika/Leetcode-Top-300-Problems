57c48ff8f6699abf4a9edc24893fedd4eb3ac08f
anikatasnin <jarintasnin09@gmail.com>
2025-07-06 23:05:20 +0600

    Add Time Needed to Buy Tickets

--- Queues/Time Needed to Buy Tickets/TimeNeededToBuyTickets.cpp
+++ Queues/Time Needed to Buy Tickets/TimeNeededToBuyTickets.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        int n = tickets.size();
 
+        for (int i = 0; i < n; ++i) {
+            if (i <= k) {
+                time += min(tickets[i], tickets[k]);
+            }
+            else {
+                time += min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return time;
+    }
+};
