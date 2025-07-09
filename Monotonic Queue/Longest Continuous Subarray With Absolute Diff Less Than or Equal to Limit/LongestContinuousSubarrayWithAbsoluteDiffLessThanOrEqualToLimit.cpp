4cb4d8257ac3a4e0b3701102a1b6f0c6cabd2782
anikatasnin <jarintasnin09@gmail.com>
2025-07-09 23:42:22 +0600

    Add Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- Monotonic Queue/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.cpp
+++ Monotonic Queue/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        deque<int> maxDeque, minDeque;
+        int start = 0, maxLength = 0;
 
+        for(int end = 0; end < nums.size(); end++){
+            while(!maxDeque.empty() && nums[maxDeque.back()] <= nums[end]){
+                maxDeque.pop_back();
+            }
+            maxDeque.push_back(end);
+
+            while(!minDeque.empty() && nums[minDeque.back()] >= nums[end]){
+                minDeque.pop_back();
+            }
+            minDeque.push_back(end);
+
+            while(nums[maxDeque.front()] - nums[minDeque.front()] > limit){
+                start++;
+                if (maxDeque.front() < start) maxDeque.pop_front();
+                if (minDeque.front() < start) minDeque.pop_front();
+            }
+
+            maxLength = std::max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+};
