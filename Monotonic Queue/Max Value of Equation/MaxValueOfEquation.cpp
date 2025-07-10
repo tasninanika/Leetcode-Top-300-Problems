918071c1662f0a3794652e83462ccd6f16de6439
anikatasnin <jarintasnin09@gmail.com>
2025-07-11 00:27:46 +0600

    Add Max Value of Equation

--- Monotonic Queue/Max Value of Equation/MaxValueOfEquation.cpp
+++ Monotonic Queue/Max Value of Equation/MaxValueOfEquation.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, int>> maxHeap;
+        int maxValue = INT_MIN;
 
+        for(const auto& point : points){
+            int xj = point[0], yj = point[1];
+
+            while(!maxHeap.empty() && (xj - maxHeap.top().second > k)){
+                maxHeap.pop();
+            }
+
+            if(!maxHeap.empty()){
+                int potentialMax = yj + xj + maxHeap.top().first;
+                maxValue = max(maxValue, potentialMax);
+            }
+
+            maxHeap.emplace(yj - xj, xj);
+        }
+
+        return maxValue;
+    }
+};
