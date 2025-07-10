e9ab050bd74a23b8a50480a5b78e3ac70fef4232
anikatasnin <jarintasnin09@gmail.com>
2025-07-10 23:55:48 +0600

    Add Jump Game VI

--- Monotonic Queue/Jump Game VI/JumpGameVI.cpp
+++ Monotonic Queue/Jump Game VI/JumpGameVI.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> dp(n);
+
+        dp[0] = nums[0];
+        dq.push_back(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!dq.empty() && dq.front() < i - k) {
+                dq.pop_front();
+            }
+
+            dp[i] = nums[i] + dp[dq.front()];
+
+            while (!dq.empty() && dp[i] >= dp[dq.back()]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return dp[n - 1];
+    }
+};
