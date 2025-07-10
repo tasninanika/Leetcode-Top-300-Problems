d42baa3fbabe056f9c09e545939ea6f8b3fc78fa
anikatasnin <jarintasnin09@gmail.com>
2025-07-10 23:58:13 +0600

    Add Sliding Window Maximum

--- Monotonic Queue/Sliding Window Maximum/SlidingWindowMaximum.cpp
+++ Monotonic Queue/Sliding Window Maximum/SlidingWindowMaximum.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> deq;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!deq.empty() && deq.front() == i - k) {
+                deq.pop_front();
+            }
+            while (!deq.empty() && nums[deq.back()] < nums[i]) {
+                deq.pop_back();
+            }
+            deq.push_back(i);
 
+            if (i >= k - 1) {
+                result.push_back(nums[deq.front()]);
+            }
+        }
+        return result;
+    }
+};
