d2842c6115703149b41a4757c8ca199cba54e74f
anikatasnin <jarintasnin09@gmail.com>
2025-07-11 20:08:03 +0600

    Add Sort Characters By Frequency

--- Bucket Sort/Sort Characters By Frequency/SortCharactersByFrequency.cpp
+++ Bucket Sort/Sort Characters By Frequency/SortCharactersByFrequency.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freqMap;
+        for (char c : s) {
+            freqMap[c]++;
+        }
 
+        int maxFreq = 0;
+        for (auto& pair : freqMap) {
+            maxFreq = max(maxFreq, pair.second);
+        }
+
+        vector<vector<char>> buckets(maxFreq + 1);
+        for (auto& pair : freqMap) {
+            buckets[pair.second].push_back(pair.first);
+        }
+
+        string result;
+        for (int i = maxFreq; i > 0; i--) {
+            for (char c : buckets[i]) {
+                result.append(i, c);
+            }
+        }
+
+        return result;
+    }
+};
