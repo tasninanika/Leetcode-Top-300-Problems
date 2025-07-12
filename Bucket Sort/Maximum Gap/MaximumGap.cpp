2f08a5b4c336ea3083527b27d74df4d5ae2e9c38
anikatasnin <jarintasnin09@gmail.com>
2025-07-12 12:13:33 +0600

    Add Maximum Gap

--- Bucket Sort/Maximum Gap/MaximumGap.cpp
+++ Bucket Sort/Maximum Gap/MaximumGap.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return 0;
 
+        sort(nums.begin(), nums.end());
+
+        int maxGap = 0;
+        for (int i = 1; i < n; i++) {
+            maxGap = max(maxGap, nums[i] - nums[i - 1]);
+        }
+        return maxGap;
+    }
+};
