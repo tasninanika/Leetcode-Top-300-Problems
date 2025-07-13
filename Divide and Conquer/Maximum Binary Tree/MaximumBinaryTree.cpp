c6ac8d2abd8efd2e8cfe7ae0e4da8733290a8ff3
anikatasnin <jarintasnin09@gmail.com>
2025-07-13 18:56:36 +0600

    Add Maximum Binary Tree

--- Divide and Conquer/Maximum Binary Tree/MaximumBinaryTree.cpp
+++ Divide and Conquer/Maximum Binary Tree/MaximumBinaryTree.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        if (nums.empty())
+            return nullptr;
 
+        int maxIndex = max_element(nums.begin(), nums.end()) - nums.begin();
+
+        TreeNode* root = new TreeNode(nums[maxIndex]);
+
+        vector<int> left(nums.begin(), nums.begin() + maxIndex);
+        root->left = constructMaximumBinaryTree(left);
+
+        vector<int> right(nums.begin() + maxIndex + 1, nums.end());
+        root->right = constructMaximumBinaryTree(right);
+
+        return root;
+    }
+};
