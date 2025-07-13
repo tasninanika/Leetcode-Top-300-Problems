3aed02e88bd4e94f180028866a414b5181925d8e
anikatasnin <jarintasnin09@gmail.com>
2025-07-13 18:45:38 +0600

    Add Pow(x, n)

--- Recursion/Pow(x, n)/Pow(x, n).cpp
+++ Recursion/Pow(x, n)/Pow(x, n).cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double res = 1.0;
+        while (N) {
+            if (N % 2 == 1) {
+                res *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        return res;
+    }
+};
