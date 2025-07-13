4b0a0aad83a5509aec4e5af41fac0230c730ae37
anikatasnin <jarintasnin09@gmail.com>
2025-07-13 18:51:07 +0600

    Add Convert Sorted List to Binary Search Tree

--- Divide and Conquer/Convert Sorted List to Binary Search Tree/ConvertSortedListToBinarySearchTree.cpp
+++ Divide and Conquer/Convert Sorted List to Binary Search Tree/ConvertSortedListToBinarySearchTree.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return nullptr;
 
+        auto findMiddle = [](ListNode* start) {
+            ListNode *prevPtr = nullptr, *slowPtr = start, *fastPtr = start;
+
+            while (fastPtr && fastPtr->next) {
+                prevPtr = slowPtr;
+                slowPtr = slowPtr->next;
+                fastPtr = fastPtr->next->next;
+            }
+            if (prevPtr) prevPtr->next = nullptr;
+
+            return slowPtr;
+        };
+
+        ListNode* mid = findMiddle(head);
+
+        TreeNode* root = new TreeNode(mid->val);
+
+        if (head == mid) {
+            return root;
+        }
+
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+
+        return root;
+    }
+};
