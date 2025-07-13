2d6d61ba3011f1fa294e195c0ed4168cbb26962e
anikatasnin <jarintasnin09@gmail.com>
2025-07-13 18:48:16 +0600

    Add Decode String

--- Recursion/Decode String/DecodeString.cpp
+++ Recursion/Decode String/DecodeString.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    string decodeStringHelper(const string &s, int &i) {
+        string result;
 
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i])) {
+                result += s[i++];
+            }
+            else {
+                int num = 0;
+                while (i < s.length() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                i++;
+                string decodedStr = decodeStringHelper(s, i);
+                while (num-- > 0){
+                    result += decodedStr;
+                }
+                i++;
+            }
+        }
+        return result;
+    }
+
+    string decodeString(string s) {
+        int i = 0;
+        return decodeStringHelper(s, i);
+    }
+};
