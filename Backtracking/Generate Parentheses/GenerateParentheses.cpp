8fb8f8aed4e7c64f3ea630b8d33bd3cfca717e2f
anikatasnin <jarintasnin09@gmail.com>
2025-07-17 18:45:19 +0600

    Add Generate Parentheses

--- Backtracking/Generate Parentheses/GenerateParentheses.cpp
+++ Backtracking/Generate Parentheses/GenerateParentheses.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    void backtrack(string current, int open, int close, int max, vector<string>& result) {
+        if (current.length() == max * 2) {
+            result.push_back(current);
+            return;
+        }
+        if (open < max)
+            backtrack(current + "(", open + 1, close, max, result);
+        if (close < open)
+            backtrack(current + ")", open, close + 1, max, result);
+    }
+
     vector<string> generateParenthesis(int n) {
 
     }
