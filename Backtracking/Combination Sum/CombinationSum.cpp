79141642021c716b99d773db887f57f197c7be14
anikatasnin <jarintasnin09@gmail.com>
2025-07-17 18:57:25 +0600

    Add Combination Sum

--- Backtracking/Combination Sum/CombinationSum.cpp
+++ Backtracking/Combination Sum/CombinationSum.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    void backtrack(vector<int>& candidates, int target, vector<int>& currentCombination, vector<vector<int>>& combinations, int start) {
+        if (target == 0) {
+            combinations.push_back(currentCombination);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); ++i) {
+            if (candidates[i] <= target) {
+                currentCombination.push_back(candidates[i]);
+                backtrack(candidates, target - candidates[i], currentCombination, combinations, i);
+                currentCombination.pop_back();
+            }
+        }
+    }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 
     }
