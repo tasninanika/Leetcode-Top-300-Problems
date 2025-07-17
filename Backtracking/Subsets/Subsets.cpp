59980e7c5431d37a3fc48a55e4203f3654ec8fbc
anikatasnin <jarintasnin09@gmail.com>
2025-07-17 18:53:57 +0600

    Add Subsets

--- Backtracking/Subsets/Subsets.cpp
+++ Backtracking/Subsets/Subsets.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& subsets) {
+        subsets.push_back(current);
+
+        for (int i = start; i < nums.size(); ++i) {
+            current.push_back(nums[i]);
+            backtrack(i + 1, nums, current, subsets);
+            current.pop_back();
+        }
+}
+
+
     vector<vector<int>> subsets(vector<int>& nums) {
 
     }
