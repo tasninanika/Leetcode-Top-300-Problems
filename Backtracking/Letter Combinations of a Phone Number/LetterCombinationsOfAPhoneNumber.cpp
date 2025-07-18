e2e3aff6f202227eba9643bb88e4036e946333a7
anikatasnin <jarintasnin09@gmail.com>
2025-07-18 22:14:33 +0600

    Add Letter Combinations of a Phone Number

--- Backtracking/Letter Combinations of a Phone Number/LetterCombinationsOfAPhoneNumber.cpp
+++ Backtracking/Letter Combinations of a Phone Number/LetterCombinationsOfAPhoneNumber.cpp
@@ -1,1 +1,43 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
 
+        const vector<string> keypad = {
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+        };
+
+        vector<string> results;
+        string currentCombination;
+
+        backtrack(digits, 0, keypad, currentCombination, results);
+
+        return results;
+    }
+
+private:
+    void backtrack(const string& digits, int index, const vector<string>& keypad, string& currentCombination, vector<string>& results) {
+        if (index == digits.size()) {
+            results.push_back(currentCombination);
+            return;
+        }
+
+        int digit = digits[index] - '0';
+        const string& possibleChars = keypad[digit];
+
+        for (char ch : possibleChars) {
+            currentCombination.push_back(ch);
+            backtrack(digits, index + 1, keypad, currentCombination, results);
+            currentCombination.pop_back();
+        }
+    }
+};
