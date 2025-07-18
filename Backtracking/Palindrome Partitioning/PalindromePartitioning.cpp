8bde9464b3f02d24f588aac90db345a297464ecd
anikatasnin <jarintasnin09@gmail.com>
2025-07-18 22:16:29 +0600

    Add Palindrome Partitioning

--- Backtracking/Palindrome Partitioning/PalindromePartitioning.cpp
+++ Backtracking/Palindrome Partitioning/PalindromePartitioning.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-
+       int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+        for (int i = 0; i+1 < n; ++i) {
+            dp[i][i+1] = (s[i] == s[i+1]);
+        }
+        for (int length = 3; length <= n; ++length) {
+            for (int i = 0; i <= n-length; ++i) {
+                int j = i + length - 1;
+                dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+            }
+        }
     }
 };
