c7136f8fe85899104ed2d5733b72977613e0f9ce
anikatasnin <jarintasnin09@gmail.com>
2025-07-20 23:54:37 +0600

    Add Binary Tree Right Side View

--- Tree Traversal - Level Order/Binary Tree Right Side View/BinaryTreeRightSideView.cpp
+++ Tree Traversal - Level Order/Binary Tree Right Side View/BinaryTreeRightSideView.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> rightView;
+        dfs(root, 0, rightView);
+        return rightView;
+    }
 
+private:
+    void dfs(TreeNode* node, int level, vector<int>& rightView) {
+        if (node == nullptr) return;
+
+        if (level == rightView.size())
+            rightView.push_back(node->val);
+
+        dfs(node->right, level + 1, rightView);
+        dfs(node->left, level + 1, rightView);
+    }
+};
