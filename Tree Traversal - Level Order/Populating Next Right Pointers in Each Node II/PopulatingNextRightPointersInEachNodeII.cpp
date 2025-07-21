f44c9318d7849a634099a96781fa0e6dee8bd1d9
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 10:56:33 +0600

    Add Populating Next Right Pointers in Each Node II

--- Tree Traversal - Level Order/Populating Next Right Pointers in Each Node II/PopulatingNextRightPointersInEachNodeII.cpp
+++ Tree Traversal - Level Order/Populating Next Right Pointers in Each Node II/PopulatingNextRightPointersInEachNodeII.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
 
+        Node* head = root;
+
+        while (head != nullptr) {
+            Node* dummy = new Node();
+            Node* tail = dummy;
+
+            for (Node* current = head; current != nullptr; current = current->next) {
+                if (current->left) {
+                    tail->next = current->left;
+                    tail = tail->next;
+                }
+                if (current->right) {
+                    tail->next = current->right;
+                    tail = tail->next;
+                }
+            }
+
+            head = dummy->next;
+            delete dummy;
+        }
+
+        return root;
+    }
+};
