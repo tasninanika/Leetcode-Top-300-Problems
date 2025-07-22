0c18263ecea5c4319fa97c68c59040a204b9bf79
anikatasnin <jarintasnin09@gmail.com>
2025-07-22 23:02:17 +0600

    Add Maximum Width of Binary Tree

--- Tree Traversal - Level Order/Maximum Width of Binary Tree/MaximumWidthOfBinaryTree.cpp
+++ Tree Traversal - Level Order/Maximum Width of Binary Tree/MaximumWidthOfBinaryTree.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    void dfs(TreeNode* node, unsigned long long index, int depth, unsigned long long &maxWidth, map<int, unsigned long long> &firstColIndexMap) {
+        if (!node) return;
 
+        if (firstColIndexMap.find(depth) == firstColIndexMap.end()) {
+            firstColIndexMap[depth] = index;
+        }
+
+        maxWidth = max(maxWidth, index - firstColIndexMap[depth] + 1);
+
+        dfs(node->left, 2 * index, depth + 1, maxWidth, firstColIndexMap);
+        dfs(node->right, 2 * index + 1, depth + 1, maxWidth, firstColIndexMap);
+    }
+
+    int widthOfBinaryTree(TreeNode* root) {
+        unsigned long long maxWidth = 0;
+        map<int, unsigned long long> firstColIndexMap;
+        dfs(root, 1, 0, maxWidth, firstColIndexMap);
+        return maxWidth;
+    }
+};
