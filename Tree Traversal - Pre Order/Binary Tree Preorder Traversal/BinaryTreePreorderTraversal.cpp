26c367bf01d99d93bae845baa14be62bdec725e9
anikatasnin <jarintasnin09@gmail.com>
2025-07-23 15:14:01 +0600

    Add Binary Tree Preorder Traversal

--- Tree Traversal - Pre Order/Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.cpp
+++ Tree Traversal - Pre Order/Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
 
+        if (root == nullptr)
+            return result;
+
+        stack<TreeNode*> nodeStack;
+        nodeStack.push(root);
+
+        while(!nodeStack.empty()) {
+            TreeNode* node = nodeStack.top();
+            nodeStack.pop();
+
+            result.push_back(node->val);
+
+            if (node->right) {
+                nodeStack.push(node->right);
+            }
+            if (node->left) {
+                nodeStack.push(node->left);
+            }
+        }
+
+        return result;
+    }
+};
