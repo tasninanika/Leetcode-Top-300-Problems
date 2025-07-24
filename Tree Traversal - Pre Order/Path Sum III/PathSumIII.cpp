5485f26fdacc631291c2d8c6fccdb0c43fec8d9d
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:44:57 +0600

    Add Path Sum III

--- Tree Traversal - Pre Order/Path Sum III/PathSumIII.cpp
+++ Tree Traversal - Pre Order/Path Sum III/PathSumIII.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int pathSumHelper(TreeNode* node, long long currentSum, int targetSum, unordered_map<long long, int>& prefixSumCount) {
+        if (!node) return 0;
+
+        currentSum += node->val;
+        int numPathsToCurr = prefixSumCount[currentSum - targetSum];
+
+        prefixSumCount[currentSum]++;
+        numPathsToCurr += pathSumHelper(node->left, currentSum, targetSum, prefixSumCount);
+        numPathsToCurr += pathSumHelper(node->right, currentSum, targetSum, prefixSumCount);
+        prefixSumCount[currentSum]--;
+
+        return numPathsToCurr;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long long, int> prefixSumCount;
+        prefixSumCount[0] = 1;
+        return pathSumHelper(root, 0, targetSum, prefixSumCount);
+    }
+};
 
