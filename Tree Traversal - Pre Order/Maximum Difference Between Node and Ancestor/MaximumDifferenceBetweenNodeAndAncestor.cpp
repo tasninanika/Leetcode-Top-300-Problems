74e76ca8090b442eb91d84f1f4ac2a17b285c0c3
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:48:10 +0600

    Add Maximum Difference Between Node and Ancestor

--- Tree Traversal - Pre Order/Maximum Difference Between Node and Ancestor/MaximumDifferenceBetweenNodeAndAncestor.cpp
+++ Tree Traversal - Pre Order/Maximum Difference Between Node and Ancestor/MaximumDifferenceBetweenNodeAndAncestor.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root, root->val, root->val);
+    }
 
+private:
+    int dfs(TreeNode* node, int currentMax, int currentMin) {
+        if (!node) return 0;
+
+        int potentialDiff = max(abs(currentMax - node->val), abs(currentMin - node->val));
+
+        currentMax = max(currentMax, node->val);
+        currentMin = min(currentMin, node->val);
+
+        int leftDiff = dfs(node->left, currentMax, currentMin);
+        int rightDiff = dfs(node->right, currentMax, currentMin);
+
+        return max(potentialDiff, max(leftDiff, rightDiff));
+    }
+};
