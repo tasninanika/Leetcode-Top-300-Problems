42330ad07fb3ef2306b81fafb964d111c41bd94c
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:36:08 +0600

    Add Count Complete Tree Nodes

--- Tree Traversal - Pre Order/Count Complete Tree Nodes/CountCompleteTreeNodes.cpp
+++ Tree Traversal - Pre Order/Count Complete Tree Nodes/CountCompleteTreeNodes.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int computeTreeHeight(TreeNode* node) {
+        int height = 0;
+        while (node != nullptr) {
+            height++;
+            node = node->left;
+        }
+        return height;
+    }
 
+    int countNodes(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        int leftHeight = computeTreeHeight(root->left);
+        int rightHeight = computeTreeHeight(root->right);
+
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) + countNodes(root->right);
+        }
+        else {
+            return (1 << rightHeight) + countNodes(root->left);
+        }
+    }
+};
