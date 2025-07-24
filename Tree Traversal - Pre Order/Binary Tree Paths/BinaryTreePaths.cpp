288eb07e858e4cad0015fa740079915c7efe0487
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:26:41 +0600

    Add Binary Tree Paths

--- Tree Traversal - Pre Order/Binary Tree Paths/BinaryTreePaths.cpp
+++ Tree Traversal - Pre Order/Binary Tree Paths/BinaryTreePaths.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> paths;
+        if (root == nullptr) return paths;
+        traverseTree(root, "", paths);
+        return paths;
+    }
 
+private:
+    void traverseTree(TreeNode* node, string path, vector<string>& paths) {
+        if (!node) return;
+
+        path += to_string(node->val);
+
+        if (!node->left && !node->right) {
+            paths.push_back(path);
+        } else {
+            path += "->";
+            traverseTree(node->left, path, paths);
+            traverseTree(node->right, path, paths);
+        }
+    }
+};
