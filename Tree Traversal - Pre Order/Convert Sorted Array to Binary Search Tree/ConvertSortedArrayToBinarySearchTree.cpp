cae4bddd5f781d45922737a8c476ffa4ad2e71e2
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:29:59 +0600

    Add Convert Sorted Array to Binary Search Tree

--- Tree Traversal - Pre Order/Convert Sorted Array to Binary Search Tree/ConvertSortedArrayToBinarySearchTree.cpp
+++ Tree Traversal - Pre Order/Convert Sorted Array to Binary Search Tree/ConvertSortedArrayToBinarySearchTree.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    TreeNode* convertToBST(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
 
+        int mid = left + (right - left) / 2;
+        TreeNode* node = new TreeNode(nums[mid]);
+
+        node->left = convertToBST(nums, left, mid - 1);
+        node->right = convertToBST(nums, mid + 1, right);
+
+        return node;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return convertToBST(nums, 0, nums.size() - 1);
+    }
+};
