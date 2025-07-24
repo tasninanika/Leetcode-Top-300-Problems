d7c326269ac9c5200a7a20a598b73fd11b353e5a
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:53:00 +0600

    Add Construct Binary Tree from Inorder and Postorder Traversal

--- Tree Traversal - Pre Order/Construct Binary Tree from Inorder and Postorder Traversal/ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
+++ Tree Traversal - Pre Order/Construct Binary Tree from Inorder and Postorder Traversal/ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -1,1 +1,32 @@
+class Solution2 {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.empty()) return nullptr;
 
+        stack<TreeNode*> s;
+        TreeNode* root = new TreeNode(postorder.back());
+        s.push(root);
+        postorder.pop_back();
+
+        while (!postorder.empty()) {
+            TreeNode* node = new TreeNode(postorder.back());
+            postorder.pop_back();
+
+            if (!s.empty() && s.top()->val != inorder.back()) {
+                s.top()->right = node;
+            } else {
+                TreeNode* parent = nullptr;
+                while (!s.empty() && s.top()->val == inorder.back()) {
+                    parent = s.top();
+                    s.pop();
+                    inorder.pop_back();
+                }
+                parent->left = node;
+            }
+
+            s.push(node);
+        }
+
+        return root;
+    }
+};
