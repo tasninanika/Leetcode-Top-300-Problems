61b1c1a06185e4792da64255e04ef9456e95475f
anikatasnin <jarintasnin09@gmail.com>
2025-07-24 18:56:44 +0600

    Add Serialize and Deserialize Binary Tree

--- Tree Traversal - Pre Order/Serialize and Deserialize Binary Tree/SerializeAndDeserializeBinaryTree.cpp
+++ Tree Traversal - Pre Order/Serialize and Deserialize Binary Tree/SerializeAndDeserializeBinaryTree.cpp
@@ -1,1 +1,50 @@
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        if (!root) return "#";
+        string result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node) {
+                result += to_string(node->val) + ",";
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                result += "#,";
+            }
+        }
+        return result;
+    }
+
+    TreeNode* deserialize(string data) {
+        if (data == "#") return nullptr;
+        stringstream ss(data);
+        string value;
+        getline(ss, value, ',');
+        TreeNode* root = new TreeNode(stoi(value));
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (getline(ss, value, ',')) {
+                if (value != "#") {
+                    node->left = new TreeNode(stoi(value));
+                    q.push(node->left);
+                }
+            }
+            if (getline(ss, value, ',')) {
+                if (value != "#") {
+                    node->right = new TreeNode(stoi(value));
+                    q.push(node->right);
+                }
+            }
+        }
+        return root;
+    }
+};
 
