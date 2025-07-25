e48f904559e3b82aa7e37e300434d98b994cedf4
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:43:10 +0600

    Add Validate Binary Search Tree

--- Tree Traversal - In Order/Validate Binary Search Tree/ValidateBinarySearchTree.cpp
+++ Tree Traversal - In Order/Validate Binary Search Tree/ValidateBinarySearchTree.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (!root) return true;
 
+        std::stack<TreeNode*> stack;
+        TreeNode* current = root;
+        TreeNode* prev = nullptr;
+
+        while (current || !stack.empty()) {
+            while (current) {
+                stack.push(current);
+                current = current->left;
+            }
+            current = stack.top();
+            stack.pop();
+            if (prev && current->val <= prev->val) return false;
+            prev = current;
+            current = current->right;
+        }
+        return true;
+    }
+};
