7153119fb6bdc2904907a626979b0a5aa6622a7a
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:38:39 +0600

    Add Minimum Distance Between BST Nodes

--- Tree Traversal - In Order/Minimum Distance Between BST Nodes/MinimumDistanceBetweenBSTNodes.cpp
+++ Tree Traversal - In Order/Minimum Distance Between BST Nodes/MinimumDistanceBetweenBSTNodes.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        int minDiff = INT_MAX;
+        int prevVal = -1;
+
+        function<void(TreeNode*)> inorder = [&](TreeNode* node) {
+            if (!node) return;
+            inorder(node->left);
+            if (prevVal != -1) {
+                minDiff = min(minDiff, node->val - prevVal);
+            }
+            prevVal = node->val;
+            inorder(node->right);
+        };
+
+        inorder(root);
+        return minDiff;
+    }
+};
 
