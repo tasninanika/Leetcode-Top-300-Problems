9e6aefbc65c55cbac8bd42462ba082a685037b54
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:47:10 +0600

    Add Binary Search Tree Iterator

--- Tree Traversal - In Order/Binary Search Tree Iterator/BinarySearchTreeIterator.cpp
+++ Tree Traversal - In Order/Binary Search Tree Iterator/BinarySearchTreeIterator.cpp
@@ -1,1 +1,29 @@
+class BSTIterator {
+private:
+    stack<TreeNode*> nodeStack;
 
+    void pushLeft(TreeNode* root) {
+        while (root) {
+            nodeStack.push(root);
+            root = root->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+
+    int next() {
+        TreeNode* node = nodeStack.top();
+        nodeStack.pop();
+        if (node->right) {
+            pushLeft(node->right);
+        }
+        return node->val;
+    }
+
+    bool hasNext() {
+        return !nodeStack.empty();
+    }
+};
