324a73564295cbd5b2d9ebddda5e3e9bcdb0fe5d
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:45:13 +0600

    Add Kth Smallest Element in a BST

--- Tree Traversal - In Order/Kth Smallest Element in a BST/KthSmallestElementInABST.cpp
+++ Tree Traversal - In Order/Kth Smallest Element in a BST/KthSmallestElementInABST.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int inorderTraversal(TreeNode* root, int& k) {
+        if (root == nullptr) return -1;
 
+        int left = inorderTraversal(root->left, k);
+        if (k == 0) return left;
+
+        k--;
+        if (k == 0) return root->val;
+
+        return inorderTraversal(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        return inorderTraversal(root, k);
+    }
+};
