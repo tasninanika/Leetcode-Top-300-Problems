44bb7c82c8e44ca39f08f128622b1bb98f439e76
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:36:36 +0600

    Add Binary Tree Inorder Traversal

--- Tree Traversal - In Order/Binary Tree Inorder Traversal/BinaryTreeInorderTraversal.cpp
+++ Tree Traversal - In Order/Binary Tree Inorder Traversal/BinaryTreeInorderTraversal.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    void inorderTraversalHelper(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) return;
+        inorderTraversalHelper(root->left, result);
+        result.push_back(root->val);
+        inorderTraversalHelper(root->right, result);
+    }
 
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorderTraversalHelper(root, result);
+        return result;
+    }
+};
