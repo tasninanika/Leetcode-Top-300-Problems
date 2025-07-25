d7aaa9a249079180a16959d5a9918b5721379cb1
anikatasnin <jarintasnin09@gmail.com>
2025-07-25 15:40:58 +0600

    Add Minimum Absolute Difference in BST

--- Tree Traversal - In Order/Minimum Absolute Difference in BST/MinimumAbsoluteDifferenceInBST.cpp
+++ Tree Traversal - In Order/Minimum Absolute Difference in BST/MinimumAbsoluteDifferenceInBST.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    void inOrderTraversal(TreeNode* root, vector<int>& vals) {
+        if (!root) return;
+        inOrderTraversal(root->left, vals);
+        vals.push_back(root->val);
+        inOrderTraversal(root->right, vals);
+    }
 
+    int getMinimumDifference(TreeNode* root) {
+        if (!root) return 0;
+
+        vector<int> vals;
+        inOrderTraversal(root, vals);
+
+        int minDiff = INT_MAX;
+        for (int i = 1; i < vals.size(); ++i) {
+            minDiff = min(minDiff, vals[i] - vals[i - 1]);
+        }
+
+        return minDiff;
+    }
+};
