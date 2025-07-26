c79030280a83cfda64641259c458e248cb119408
anikatasnin <jarintasnin09@gmail.com>
2025-07-26 15:22:01 +0600

    Add Diameter of Binary Tree

--- Tree Traversal - Post-Order/Diameter of Binary Tree/diameterOfBinaryTree.cpp
+++ Tree Traversal - Post-Order/Diameter of Binary Tree/diameterOfBinaryTree.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        return heightAndDiameter(root).second;
+    }
 
+private:
+    std::pair<int, int> heightAndDiameter(TreeNode* node) {
+        if (!node) return {0, 0};
+
+        auto left = heightAndDiameter(node->left);
+        auto right = heightAndDiameter(node->right);
+
+        int currentHeight = 1 + std::max(left.first, right.first);
+
+        int currentDiameter = std::max({left.second, right.second, left.first + right.first});
+
+        return {currentHeight, currentDiameter};
+    }
+};
