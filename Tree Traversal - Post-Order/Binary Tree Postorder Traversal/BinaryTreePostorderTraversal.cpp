8107c02cdd054eee5924f8833ec7686560d10965
anikatasnin <jarintasnin09@gmail.com>
2025-07-26 15:18:02 +0600

    Add Binary Tree Postorder Traversal

--- Tree Traversal - Post-Order/Binary Tree Postorder Traversal/BinaryTreePostorderTraversal.cpp
+++ Tree Traversal - Post-Order/Binary Tree Postorder Traversal/BinaryTreePostorderTraversal.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    void postorderTraversalHelper(TreeNode* node, vector<int>& result) {
+        if (node == nullptr)
+            return;
+        postorderTraversalHelper(node->left, result);
+        postorderTraversalHelper(node->right, result);
+        result.push_back(node->val);
+    }
 
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorderTraversalHelper(root, result);
+        return result;
+    }
+};
