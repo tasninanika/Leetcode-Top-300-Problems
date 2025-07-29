a1931b1f2a086dad03dbc998159b95b019d2e001
anikatasnin <jarintasnin09@gmail.com>
2025-07-29 20:59:55 +0600

    Add Flatten Binary Tree to Linked List

--- Tree Traversal - Post-Order/Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
+++ Tree Traversal - Post-Order/Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
 
+        while (curr) {
+            if (curr->left) {
+                TreeNode* rightmost = curr->left;
+
+                while (rightmost->right) {
+                    rightmost = rightmost->right;
+                }
+
+                rightmost->right = curr->right;
+
+                curr->right = curr->left;
+                curr->left = nullptr;
+            }
+
+            curr = curr->right;
+        }
+    }
+};
