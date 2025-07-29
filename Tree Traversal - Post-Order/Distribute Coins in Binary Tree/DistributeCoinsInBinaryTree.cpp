675046b5d72d28242b73e30d91f0df3bdab4e7ed
anikatasnin <jarintasnin09@gmail.com>
2025-07-29 21:03:50 +0600

    Add Distribute Coins in Binary Tree

--- Tree Traversal - Post-Order/Distribute Coins in Binary Tree/DistributeCoinsInBinaryTree.cpp
+++ Tree Traversal - Post-Order/Distribute Coins in Binary Tree/DistributeCoinsInBinaryTree.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int totalMoves = 0;
 
+    int distributeCoins(TreeNode* root) {
+        distribute(root);
+        return totalMoves;
+    }
+
+    int distribute(TreeNode* node) {
+        if (!node) return 0;
+
+        int leftExcess = distribute(node->left);
+        int rightExcess = distribute(node->right);
+
+        totalMoves += abs(leftExcess) + abs(rightExcess);
+
+        return node->val + leftExcess + rightExcess - 1;
+    }
+};
