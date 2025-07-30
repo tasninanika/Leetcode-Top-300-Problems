3c32f1453dcc2cf6989b925cdd7bcb21dfbfff7a
anikatasnin <jarintasnin09@gmail.com>
2025-07-30 15:22:48 +0600

    Add My Calendar II

--- BST or Ordered Set/My Calendar II/MyCalendarII.cpp
+++ BST or Ordered Set/My Calendar II/MyCalendarII.cpp
@@ -1,1 +1,20 @@
+class MyCalendarTwo {
+public:
+    map<int, int> events;
 
+    bool book(int start, int end) {
+        events[start]++;
+        events[end]--;
+
+        int activeBookings = 0;
+        for (const auto &[time, count] : events) {
+            activeBookings += count;
+            if (activeBookings > 2) {
+                events[start]--;
+                events[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
