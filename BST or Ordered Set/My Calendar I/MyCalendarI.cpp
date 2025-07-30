bb4b979a36c13d136de942d364b5611eabecc400
anikatasnin <jarintasnin09@gmail.com>
2025-07-30 15:21:02 +0600

    Add My Calendar I

--- BST or Ordered Set/My Calendar I/MyCalendarI.cpp
+++ BST or Ordered Set/My Calendar I/MyCalendarI.cpp
@@ -1,1 +1,25 @@
+class MyCalendar {
+private:
+    std::map<int, int> events;
 
+public:
+    MyCalendar() {}
+
+    bool book(int start, int end) {
+        auto next_event = events.lower_bound(start);
+
+        if (next_event != events.end() && next_event->first < end) {
+            return false;
+        }
+
+        if (next_event != events.begin()) {
+            auto prev_event = std::prev(next_event);
+            if (prev_event->second > start) {
+                return false;
+            }
+        }
+
+        events[start] = end;
+        return true;
+    }
+};
