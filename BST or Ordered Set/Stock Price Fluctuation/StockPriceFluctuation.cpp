81cb2c7dbcb161b30a92f3cc97e372a0cd53bfff
anikatasnin <jarintasnin09@gmail.com>
2025-07-30 15:24:45 +0600

    Add Stock Price Fluctuation

--- BST or Ordered Set/Stock Price Fluctuation/StockPriceFluctuation.cpp
+++ BST or Ordered Set/Stock Price Fluctuation/StockPriceFluctuation.cpp
@@ -1,1 +1,32 @@
+class StockPrice {
+private:
+    std::map<int, int> timestampPriceMap;
+    std::multiset<int> prices;
+    int maxTimestamp;
 
+public:
+    StockPrice() : maxTimestamp(0) {}
+
+    void update(int timestamp, int price) {
+        if (timestampPriceMap.find(timestamp) != timestampPriceMap.end()) {
+            prices.erase(prices.find(timestampPriceMap[timestamp]));
+        }
+        timestampPriceMap[timestamp] = price;
+        prices.insert(price);
+        if (timestamp > maxTimestamp) {
+            maxTimestamp = timestamp;
+        }
+    }
+
+    int current() {
+        return timestampPriceMap[maxTimestamp];
+    }
+
+    int maximum() {
+        return *prices.rbegin();
+    }
+
+    int minimum() {
+        return *prices.begin();
+    }
+};
