540b5e82de9df871cfb046b9564fdbec5af25c2f
anikatasnin <jarintasnin09@gmail.com>
2025-07-31 19:40:55 +0600

    Add Word Search II

--- Tries/Word Search II/WordSearchII.cpp
+++ Tries/Word Search II/WordSearchII.cpp
@@ -1,1 +1,63 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    string word;
+
+    TrieNode() : word("") {
+        for (int i = 0; i < 26; i++) children[i] = nullptr;
+    }
+};
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        TrieNode* root = buildTrie(words);
+        vector<string> result;
+
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[0].size(); ++j) {
+                dfs(board, i, j, root, result);
+            }
+        }
+
+        return result;
+    }
+
+private:
+    TrieNode* buildTrie(vector<string>& words) {
+        TrieNode* root = new TrieNode();
+        for (string& word : words) {
+            TrieNode* node = root;
+            for (char c : word) {
+                int idx = c - 'a';
+                if (!node->children[idx]) {
+                    node->children[idx] = new TrieNode();
+                }
+                node = node->children[idx];
+            }
+            node->word = word;
+        }
+        return root;
+    }
+
+    void dfs(vector<vector<char>>& board, int i, int j, TrieNode* node, vector<string>& result) {
+        char c = board[i][j];
+        if (c == '#' || !node->children[c - 'a']) return;
+
+        node = node->children[c - 'a'];
+        if (!node->word.empty()) {
+            result.push_back(node->word);
+            node->word.clear();
+        }
+
+        board[i][j] = '#';
+
+        if (i > 0) dfs(board, i - 1, j, node, result);
+        if (j > 0) dfs(board, i, j - 1, node, result);
+        if (i < board.size() - 1) dfs(board, i + 1, j, node, result);
+        if (j < board[0].size() - 1) dfs(board, i, j + 1, node, result);
+
+        board[i][j] = c;
+    }
+};
 
