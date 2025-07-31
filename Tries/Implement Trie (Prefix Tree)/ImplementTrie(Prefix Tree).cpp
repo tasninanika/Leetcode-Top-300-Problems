c12f5e8aa7e0c1e5e0e37dfb4f928cc96e96a682
anikatasnin <jarintasnin09@gmail.com>
2025-07-31 19:18:47 +0600

    Add Implement Trie (Prefix Tree)

--- Tries/Implement Trie (Prefix Tree)/ImplementTrie(Prefix Tree).cpp
+++ Tries/Implement Trie (Prefix Tree)/ImplementTrie(Prefix Tree).cpp
@@ -1,1 +1,58 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        for (int i = 0; i < 26; ++i) {
+            children[i] = nullptr;
+        }
+        isEndOfWord = false;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
 
