9a11e1ac29de2cf761dcf0b61594c1fe30c32a94
anikatasnin <jarintasnin09@gmail.com>
2025-07-31 19:27:22 +0600

    Add Longest Word in Dictionary

--- Tries/Longest Word in Dictionary/LongestWordInDictionary.cpp
+++ Tries/Longest Word in Dictionary/LongestWordInDictionary.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());
+        unordered_set<string> buildableWords;
+        buildableWords.insert("");
+        string longestWord = "";
+
+        for (const string& word : words) {
+            if (buildableWords.count(word.substr(0, word.length() - 1))) {
+                buildableWords.insert(word);
+                if (word.length() > longestWord.length()) {
+                    longestWord = word;
+                }
+            }
+        }
+        return longestWord;
+    }
+};
 
