397305ab6ef6ce607912bf4c20b533e5c119bf38
anikatasnin <jarintasnin09@gmail.com>
2025-07-31 19:32:22 +0600

    Add Maximum XOR of Two Numbers in an Array

--- Tries/Maximum XOR of Two Numbers in an Array/MaximumXOROfTwoNumbersInAnArray.cpp
+++ Tries/Maximum XOR of Two Numbers in an Array/MaximumXOROfTwoNumbersInAnArray.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int findMaximumXOR(std::vector<int>& nums) {
+        int maxXOR = 0;
+        int n = nums.size();
 
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int xorVal = nums[i] ^ nums[j];
+                maxXOR = std::max(maxXOR, xorVal);
+            }
+        }
+
+        return maxXOR;
+    }
+
+};
