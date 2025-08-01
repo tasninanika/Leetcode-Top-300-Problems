fe8189fe2e42cb1888e9af6247bcde30335c801a
anikatasnin <jarintasnin09@gmail.com>
2025-08-01 20:18:58 +0600

    Add Furthest Building You Can Reach

--- Heaps/Furthest Building You Can Reach/FurthestBuildingYouCanReach.cpp
+++ Heaps/Furthest Building You Can Reach/FurthestBuildingYouCanReach.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n = heights.size();
-        return solve(heights, 0, bricks, ladders, n);
-    }
-
-    int solve(const vector<int>& heights, int i, int bricks, int ladders, int n) {
-        if (i == n - 1) return i;
-        int diff = heights[i + 1] - heights[i];
-        if (diff > 0) {
-            int ans = i;
-            if (bricks >= diff) {
-                ans = max(ans, solve(heights, i + 1, bricks - diff, ladders, n));
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < n - 1; ++i) {
+            int diff = heights[i + 1] - heights[i];
+            if (diff > 0) {
+                if (pq.size() < ladders) {
+                    pq.push(diff);
+                } else {
+                    if (pq.empty() || pq.top() >= diff) {
+                        bricks -= diff;
+                    } else {
+                        int poll = pq.top(); pq.pop();
+                        pq.push(diff);
+                        bricks -= poll;
+                    }
+                    if (bricks < 0) return i;
+                }
             }
-            if (ladders > 0) {
-                ans = max(ans, solve(heights, i + 1, bricks, ladders - 1, n));
-            }
-            return ans;
-        } else {
-            return solve(heights, i + 1, bricks, ladders, n);
         }
+        return n - 1;
     }
 };
