3815891985db2d48ede023c1ae0d76214e6c6821
anikatasnin <jarintasnin09@gmail.com>
2025-08-01 20:22:02 +0600

    Add Single-Threaded CPU

--- Heaps/Single-Threaded CPU/Single-ThreadedCPU.cpp
+++ Heaps/Single-Threaded CPU/Single-ThreadedCPU.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+        vector<vector<int>> indexedTasks;
+        for (int i = 0; i < n; i++) {
+            indexedTasks.push_back({tasks[i][0], tasks[i][1], i});
+        }
+
+        sort(indexedTasks.begin(), indexedTasks.end());
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> result;
+        long long currTime = 0;
+        int index = 0;
+
+        while (index < n || !pq.empty()) {
+            while (index < n && indexedTasks[index][0] <= currTime) {
+                pq.push({indexedTasks[index][1], indexedTasks[index][2]});
+                index++;
+            }
+
+            if (!pq.empty()) {
+                auto [processingTime, taskIndex] = pq.top();
+                pq.pop();
+                currTime += processingTime;
+                result.push_back(taskIndex);
+            } else if (index < n) {
+                currTime = indexedTasks[index][0];
+            }
+        }
+
+        return result;
+    }
+};
 
