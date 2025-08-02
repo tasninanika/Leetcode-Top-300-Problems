be74b077534f48d41b6a30d9f12fa439079ae0ef
anikatasnin <jarintasnin09@gmail.com>
2025-08-02 18:52:21 +0600

    Add Find Median from Data Stream

--- Two Heaps/Find Median from Data Stream/FindMedianfromDataStream.cpp
+++ Two Heaps/Find Median from Data Stream/FindMedianfromDataStream.cpp
@@ -1,1 +1,27 @@
+class MedianFinder {
+private:
+    std::priority_queue<int> maxHeap;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
 
+public:
+    MedianFinder() {}
+
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        } else {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        }
+    }
+};
