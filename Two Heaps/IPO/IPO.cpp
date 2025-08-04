18c9cc018d0ffd57ae8d816587965e525a70f45e
anikatasnin <jarintasnin09@gmail.com>
2025-08-04 19:26:25 +0600

    Add IPO

--- Two Heaps/IPO/IPO.cpp
+++ Two Heaps/IPO/IPO.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int W, vector<int>& Profits, vector<int>& Capital) {
+        int n = Profits.size();
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; ++i) {
+            projects.push_back({Capital[i], Profits[i]});
+        }
+        sort(projects.begin(), projects.end());
 
+        priority_queue<int> maxHeap;
+        int i = 0;
+
+        for (int j = 0; j < k; ++j) {
+            while (i < n && projects[i].first <= W) {
+                maxHeap.push(projects[i].second);
+                i++;
+            }
+            if (maxHeap.empty()) break;
+
+            W += maxHeap.top();
+            maxHeap.pop();
+        }
+
+        return W;
+    }
+};
