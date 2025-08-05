7b5fd16107411e03b9ae032ec47b5d2bc2530d06
anikatasnin <jarintasnin09@gmail.com>
2025-08-05 23:11:52 +0600

    Add Kth Largest Element in a Stream

--- Top K Elements/Kth Largest Element in a Stream/KthLargestElementInaStream.cpp
+++ Top K Elements/Kth Largest Element in a Stream/KthLargestElementInaStream.cpp
@@ -1,1 +1,23 @@
+class KthLargest {
+public:
+    KthLargest(int k, std::vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            add(num);
+        }
+    }
 
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        }
+        else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
+    }
+
+private:
+    int k;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+};
