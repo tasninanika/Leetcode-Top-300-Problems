7524ec2b4d75abbfc94d86cf4809161b39537fe1
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 12:58:36 +0600

    Add Minimum Number of Arrows to Burst Balloons

--- Intervals/Minimum Number of Arrows to Burst Balloons/MinimumNumberOfArrowsToBurstBalloons.cpp
+++ Intervals/Minimum Number of Arrows to Burst Balloons/MinimumNumberOfArrowsToBurstBalloons.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
 
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+
+        int arrows = 1;
+        int last_end = points[0][1];
+
+        for (const auto& balloon : points) {
+            if (balloon[0] > last_end) {
+                arrows++;
+                last_end = balloon[1];
+            }
+        }
+
+        return arrows;
+    }
+};
