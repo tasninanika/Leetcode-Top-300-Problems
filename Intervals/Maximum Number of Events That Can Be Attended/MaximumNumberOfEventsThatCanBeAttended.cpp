594cdb3185878f241b16b39e7bec4f1e78480b76
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 13:30:31 +0600

    Add Minimum Number of Arrows to Burst Balloons

--- Intervals/Maximum Number of Events That Can Be Attended/MaximumNumberOfEventsThatCanBeAttended.cpp
+++ Intervals/Maximum Number of Events That Can Be Attended/MaximumNumberOfEventsThatCanBeAttended.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-    });
+        sort(events.begin(), events.end());
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-        int maxAttendable = 0;
-        int lastDay = 0;
+    int maxAttendable = 0;
+    int day = events[0][0];
+    size_t i = 0;
 
-        vector<bool> isDayTaken(100001, false);
-
-        for (auto& event : events) {
-            for (int d = event[0]; d <= event[1]; ++d) {
-                if (!isDayTaken[d]) {
-                    isDayTaken[d] = true;
-                    maxAttendable++;
-                    break;
-                }
-            }
+    while (!pq.empty() || i < events.size()) {
+        while (i < events.size() && events[i][0] <= day) {
+            pq.push(events[i][1]);
+            ++i;
         }
-        return maxAttendable;
+
+        while (!pq.empty() && pq.top() < day) {
+            pq.pop();
+        }
+
+        if (!pq.empty()) {
+            pq.pop();
+            maxAttendable++;
+        }
+
+        day++;
+    }
+
+    return maxAttendable;
     }
 };
