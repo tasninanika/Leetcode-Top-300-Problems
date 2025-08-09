f411d0598654f4b24d057c97b298f6e603d4a865
anikatasnin <jarintasnin09@gmail.com>
2025-08-09 23:26:27 +0600

    Add Non-overlapping Intervals

--- Intervals/Non-overlapping Intervals/Non-overlappingIntervals.cpp
+++ Intervals/Non-overlapping Intervals/Non-overlappingIntervals.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const std::vector<int>& b) {
+            return a[1] < b[1];
+        });
 
+        int prev_end = intervals[0][1];
+        int count = 0;
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] < prev_end) {
+                ++count;
+            }
+            else {
+                prev_end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+};
