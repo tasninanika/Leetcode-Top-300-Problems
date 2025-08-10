7f75d0e39d27c0293d239d1d7fc636610fa316c6
anikatasnin <jarintasnin09@gmail.com>
2025-08-10 23:59:08 +0600

    Add Merge k Sorted Lists

--- K-Way Merge/Merge k Sorted Lists/MergekSortedLists.cpp
+++ K-Way Merge/Merge k Sorted Lists/MergekSortedLists.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto compare = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(compare)> minHeap(compare);
 
+        for (auto list : lists) {
+            if (list) {
+                minHeap.push(list);
+            }
+        }
+
+        ListNode* dummy = new ListNode();
+        ListNode* current = dummy;
+
+        while (!minHeap.empty()) {
+            ListNode* smallest = minHeap.top();
+            minHeap.pop();
+            current->next = smallest;
+            current = current->next;
+            if (smallest->next) {
+                minHeap.push(smallest->next);
+            }
+        }
+
+        return dummy->next;
+    }
+};
