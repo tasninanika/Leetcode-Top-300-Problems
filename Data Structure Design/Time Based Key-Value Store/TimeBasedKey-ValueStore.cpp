0443699796bddcd8f805db825226c85082a9632f
anikatasnin <jarintasnin09@gmail.com>
2025-08-11 23:13:04 +0600

    Add Time Based Key-Value Store

--- Data Structure Design/Time Based Key-Value Store/TimeBasedKey-ValueStore.cpp
+++ Data Structure Design/Time Based Key-Value Store/TimeBasedKey-ValueStore.cpp
@@ -1,1 +1,29 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> store;
 
+    void set(string key, string value, int timestamp) {
+        store[key].emplace_back(timestamp, value);
+    }
+
+    string get(string key, int timestamp) {
+        if (store.find(key) == store.end()) {
+            return "";
+        }
+
+        const auto& values = store[key];
+
+        auto it = upper_bound(values.begin(), values.end(), make_pair(timestamp, string("")),
+                              [](const pair<int, string>& a, const pair<int, string>& b) {
+                                  return a.first < b.first;
+                              });
+
+        if (it == values.begin()) {
+            return "";
+        }
+
+        --it;
+
+        return it->second;
+    }
+};
