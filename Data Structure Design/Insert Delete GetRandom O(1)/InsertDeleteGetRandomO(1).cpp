2f9ab63a5ab7c1d8d04d0bfc7da5d6709b9060ce
anikatasnin <jarintasnin09@gmail.com>
2025-08-13 10:15:49 +0600

    Add Insert Delete GetRandom O(1)

--- Data Structure Design/Insert Delete GetRandom O(1)/InsertDeleteGetRandomO(1).cpp
+++ Data Structure Design/Insert Delete GetRandom O(1)/InsertDeleteGetRandomO(1).cpp
@@ -1,1 +1,37 @@
+class RandomizedSet {
+private:
+    std::unordered_map<int, int> valToIndex;
+    std::vector<int> elements;
 
+public:
+    RandomizedSet() {}
+
+    bool insert(int val) {
+        if (valToIndex.find(val) != valToIndex.end()) {
+            return false;
+        }
+        elements.push_back(val);
+        valToIndex[val] = elements.size() - 1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if (valToIndex.find(val) == valToIndex.end()) {
+            return false;
+        }
+
+        int indexToRemove = valToIndex[val];
+        int lastElement = elements.back();
+        elements[indexToRemove] = lastElement;
+        valToIndex[lastElement] = indexToRemove;
+
+        elements.pop_back();
+        valToIndex.erase(val);
+        return true;
+    }
+
+    int getRandom() {
+        int randomIndex = rand() % elements.size();
+        return elements[randomIndex];
+    }
+};
