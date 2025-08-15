56e3d9f04794bdcb45010497b127c6b2ca05075e
anikatasnin <jarintasnin09@gmail.com>
2025-08-15 22:01:38 +0600

    Add Maximum Frequency Stack

--- Data Structure Design/Maximum Frequency Stack/MaximumFrequencyStack.cpp
+++ Data Structure Design/Maximum Frequency Stack/MaximumFrequencyStack.cpp
@@ -1,1 +1,31 @@
+class FreqStack {
+    std::unordered_map<int, int> freq;
+    std::unordered_map<int, std::stack<int>> group;
+    int maxFreq = 0;
 
+public:
+    FreqStack() {}
+
+    void push(int x) {
+        freq[x]++;
+
+        if (freq[x] > maxFreq) {
+            maxFreq = freq[x];
+        }
+
+        group[freq[x]].push(x);
+    }
+
+    int pop() {
+        int x = group[maxFreq].top();
+        group[maxFreq].pop();
+
+        freq[x]--;
+
+        if (group[maxFreq].empty()) {
+            maxFreq--;
+        }
+
+        return x;
+    }
+};
