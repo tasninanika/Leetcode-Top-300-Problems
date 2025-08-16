22a4db7fe2f4e28207cc4b99e506fc0d76927cd7
anikatasnin <jarintasnin09@gmail.com>
2025-08-16 18:34:33 +0600

    Add Minimum Add to Make Parentheses Valid

--- Greedy/Minimum Add to Make Parentheses Valid/MinimumAddtoMakeParenthesesValid.cpp
+++ Greedy/Minimum Add to Make Parentheses Valid/MinimumAddtoMakeParenthesesValid.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int balance = 0;
+        int min_add = 0;
 
+        for (char c : s) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                if (balance == 0) {
+                    ++min_add;
+                } else {
+                    --balance;
+                }
+            }
+        }
+
+
+        return min_add + balance;
+    }
+};
