48e3d80d0788d0bfd8c9657a20ae035563df4c75
anikatasnin <jarintasnin09@gmail.com>
2025-08-16 18:32:11 +0600

    Add Jump Game II

--- Greedy/Jump Game II/JumpGameII.c
+++ Greedy/Jump Game II/JumpGameII.c
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
 
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+
+                if (current_end >= nums.size() - 1) break;
+            }
+        }
+
+        return jumps;
+    }
+};
