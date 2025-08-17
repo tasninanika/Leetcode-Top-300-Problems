81392f17275db77bf994ec08ce7ecdfc9bf9e70a
anikatasnin <jarintasnin09@gmail.com>
2025-08-17 23:45:40 +0600

    Add Gas Station

--- Greedy/Gas Station/GasStation.cpp
+++ Greedy/Gas Station/GasStation.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
+        int totalGas = 0, currentGas = 0, startStation = 0;
 
+        for (int i = 0; i < gas.size(); ++i) {
+            totalGas += gas[i] - cost[i];
+            currentGas += gas[i] - cost[i];
+
+            if (currentGas < 0) {
+                startStation = i + 1;
+                currentGas = 0;
+            }
+        }
+
+        return totalGas >= 0 ? startStation : -1;
+    }
+};
