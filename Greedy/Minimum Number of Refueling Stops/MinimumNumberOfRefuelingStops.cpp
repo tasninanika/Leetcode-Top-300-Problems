8f1fa159b67383ad7d1091eb597568d3bd1c209e
anikatasnin <jarintasnin09@gmail.com>
2025-08-19 23:18:13 +0600

    Add Candy

--- Greedy/Minimum Number of Refueling Stops/MinimumNumberOfRefuelingStops.cpp
+++ Greedy/Minimum Number of Refueling Stops/MinimumNumberOfRefuelingStops.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        priority_queue<int> maxHeap;
 
+        stations.push_back({target, 0});
+
+        int currentFuel = startFuel;
+        int stops = 0;
+        int prevPosition = 0;
+
+        for (const auto& station : stations) {
+            int stationPosition = station[0];
+            int stationFuel = station[1];
+
+            while (currentFuel < stationPosition - prevPosition) {
+                if (maxHeap.empty()) return -1;
+                currentFuel += maxHeap.top();
+                maxHeap.pop();
+                stops++;
+            }
+
+            currentFuel -= (stationPosition - prevPosition);
+            prevPosition = stationPosition;
+            maxHeap.push(stationFuel);
+        }
+
+        return stops;
+    }
+};
