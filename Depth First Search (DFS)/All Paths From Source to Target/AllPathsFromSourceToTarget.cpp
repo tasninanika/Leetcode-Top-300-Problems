c8f55970a883b8a1d76c515ad6b21c75b332e715
anikatasnin <jarintasnin09@gmail.com>
2025-08-20 11:02:28 +0600

    Add All Paths From Source to Target

--- Depth First Search (DFS)/All Paths From Source to Target/AllPathsFromSourceToTarget.cpp
+++ Depth First Search (DFS)/All Paths From Source to Target/AllPathsFromSourceToTarget.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        stack<pair<int, vector<int>>> s;
+        s.push({0, {0}});
+
+        while (!s.empty()) {
+            auto [node, path] = s.top();
+            s.pop();
+
+            if (node == graph.size() - 1) {
+                result.push_back(path);
+            } else {
+                for (int nextNode : graph[node]) {
+                    vector<int> newPath = path;
+                    newPath.push_back(nextNode);
+                    s.push({nextNode, newPath});
+                }
+            }
+        }
+
+        return result;
+    }
+};
 
