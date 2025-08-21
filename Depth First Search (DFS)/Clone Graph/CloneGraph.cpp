ef146a9d9ad24b3891cb4990ae455f14b7f9294e
anikatasnin <jarintasnin09@gmail.com>
2025-08-21 23:56:02 +0600

    Add Clone Graph

--- Depth First Search (DFS)/Clone Graph/CloneGraph.cpp
+++ Depth First Search (DFS)/Clone Graph/CloneGraph.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
 
+        unordered_map<Node*, Node*> visited;
+
+        queue<Node*> q;
+
+        Node* clone = new Node(node->val);
+        visited[node] = clone;
+        q.push(node);
+
+        while (!q.empty()) {
+            Node* current = q.front();
+            q.pop();
+
+            for (auto neighbor : current->neighbors) {
+                if (!visited.count(neighbor)) {
+                    visited[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                visited[current]->neighbors.push_back(visited[neighbor]);
+            }
+        }
+
+        return clone;
+    }
+};
