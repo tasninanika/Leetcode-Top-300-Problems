d1966d4a88784a4d26c2a9a460d5e02cb3c3f2aa
anikatasnin <jarintasnin09@gmail.com>
2025-08-22 08:09:45 +0600

    Add Is Graph Bipartite

--- Depth First Search (DFS)/Is Graph Bipartite/IsGraphBipartite.cpp
+++ Depth First Search (DFS)/Is Graph Bipartite/IsGraphBipartite.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph, vector<int>& color, int node, int currentColor) {
+        color[node] = currentColor;
 
+        for (int adjacent : graph[node]) {
+            if (color[adjacent] == -1) {
+                if (!dfs(graph, color, adjacent, 1 - currentColor)) {
+                    return false;
+                }
+            }
+            else if (color[adjacent] == color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+
+        for (int i = 0; i < n; ++i) {
+            if (color[i] == -1) {
+                if (!dfs(graph, color, i, 0)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
