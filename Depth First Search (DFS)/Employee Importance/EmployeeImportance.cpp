3cdc79b6447fca8aed0059a858332851ccc9ec87
anikatasnin <jarintasnin09@gmail.com>
2025-08-23 07:02:59 +0600

    Add Employee Importance

--- Depth First Search (DFS)/Employee Importance/EmployeeImportance.cpp
+++ Depth First Search (DFS)/Employee Importance/EmployeeImportance.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> empMap;
+        for (auto& emp : employees) {
+            empMap[emp->id] = emp;
+        }
 
+        return dfs(id, empMap);
+    }
+
+    int dfs(int id, unordered_map<int, Employee*>& empMap) {
+        Employee* emp = empMap[id];
+        int total_importance = emp->importance;
+
+        for (int subId : emp->subordinates) {
+            total_importance += dfs(subId, empMap);
+        }
+
+        return total_importance;
+    }
+};
