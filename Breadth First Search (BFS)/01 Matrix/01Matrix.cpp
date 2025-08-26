f0afc84092eccf7bd28ad35d0b460fb115707280
anikatasnin <jarintasnin09@gmail.com>
2025-08-26 09:18:53 +0600

    Add 01 Matrix

--- Breadth First Search (BFS)/01 Matrix/01Matrix.cpp
+++ Breadth First Search (BFS)/01 Matrix/01Matrix.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+
+            for (auto& dir : directions) {
+                int ni = i + dir[0], nj = j + dir[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                    if (dist[ni][nj] > dist[i][j] + 1) {
+                        dist[ni][nj] = dist[i][j] + 1;
+                        q.push({ni, nj});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+};
