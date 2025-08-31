144eaf789e3f475accbc510b25c3719ffc68883d
anikatasnin <jarintasnin09@gmail.com>
2025-08-31 18:47:44 +0600

    Add Minimum Height Trees

--- Topological Sort/Minimum Height Trees/MinimumHeightTrees.cpp
+++ Topological Sort/Minimum Height Trees/MinimumHeightTrees.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+   vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return {0};
 
+        vector<vector<int>> adj(n);
+        vector<int> degree(n, 0);
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+
+        queue<int> leaves;
+        for (int i = 0; i < n; ++i) {
+            if (degree[i] == 1) {
+                leaves.push(i);
+            }
+        }
+
+        int remainingNodes = n;
+        while (remainingNodes > 2) {
+            int leavesSize = leaves.size();
+            remainingNodes -= leavesSize;
+            for (int i = 0; i < leavesSize; ++i) {
+                int leaf = leaves.front();
+                leaves.pop();
+                for (int neighbor : adj[leaf]) {
+                    degree[neighbor]--;
+                    if (degree[neighbor] == 1) {
+                        leaves.push(neighbor);
+                    }
+                }
+            }
+        }
+
+        vector<int> mhtRoots;
+        while (!leaves.empty()) {
+            mhtRoots.push_back(leaves.front());
+            leaves.pop();
+        }
+
+        return mhtRoots;
+    }
+};
