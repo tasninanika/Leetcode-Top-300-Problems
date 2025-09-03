202105acc4d68065cd0e2e4e1d3331d164dbbc77
anikatasnin <jarintasnin09@gmail.com>
2025-09-03 10:08:44 +0600

    Add Redundant Connection

--- Union Find/Redundant Connection/RedundantConnection.cpp
+++ Union Find/Redundant Connection/RedundantConnection.cpp
@@ -1,1 +1,51 @@
+class Solution {
+public:
+    int find(vector<int>& parent, int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent, parent[x]);
+        }
+        return parent[x];
+    }
 
+    void unionSets(vector<int>& parent, vector<int>& rank, int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            }
+            else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            }
+            else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> parent(n + 1);
+        vector<int> rank(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (const auto& edge : edges) {
+            int rootX = find(parent, edge[0]);
+            int rootY = find(parent, edge[1]);
+
+            if (rootX == rootY) {
+                return edge;
+            }
+            else {
+                unionSets(parent, rank, rootX, rootY);
+            }
+        }
+
+        return {};
+    }
+};
