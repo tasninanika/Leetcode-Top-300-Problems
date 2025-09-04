826f4c38b4da42b22717f8e53d4f6e211dfecb67
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 20:13:38 +0600

    Add Network Delay Time

--- Shortest Path/Network Delay Time/NetworkDelayTime.cpp
+++ Shortest Path/Network Delay Time/NetworkDelayTime.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int> distance(n + 1, numeric_limits<int>::max());
+        distance[k] = 0;
 
+        for (int i = 0; i < n - 1; ++i) {
+            for (auto& edge : times) {
+                int u = edge[0], v = edge[1], w = edge[2];
+                if (distance[u] != numeric_limits<int>::max() && distance[u] + w < distance[v]) {
+                    distance[v] = distance[u] + w;
+                }
+            }
+        }
+
+        int maxTime = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (distance[i] == numeric_limits<int>::max())
+                return -1;
+            maxTime = max(maxTime, distance[i]);
+        }
+
+        return maxTime;
+    }
+};
