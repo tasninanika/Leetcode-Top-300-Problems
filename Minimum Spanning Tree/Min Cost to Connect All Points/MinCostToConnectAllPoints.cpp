c638c9ce4ff8ced190418d77cc59cf5eee5d8d75
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 20:11:06 +0600

    Add Min Cost to Connect All Points

--- Minimum Spanning Tree/Min Cost to Connect All Points/MinCostToConnectAllPoints.cpp
+++ Minimum Spanning Tree/Min Cost to Connect All Points/MinCostToConnectAllPoints.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        if (n == 0) return 0;
 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        vector<bool> inMST(n, false);
+
+        minHeap.push({0, 0});
+        int totalCost = 0;
+        int edgesUsed = 0;
+
+        while (edgesUsed < n && !minHeap.empty()) {
+            auto [cost, u] = minHeap.top();
+            minHeap.pop();
+
+            if (inMST[u]) continue;
+
+            inMST[u] = true;
+            totalCost += cost;
+            edgesUsed++;
+
+            for (int v = 0; v < n; ++v) {
+                if (!inMST[v]) {
+                    int nextCost = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                    minHeap.push({nextCost, v});
+                }
+            }
+        }
+
+        return totalCost;
+    }
+};
