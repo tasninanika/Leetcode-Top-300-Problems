e437c09d2d00144d1f595003cdb44449c085fe8b
anikatasnin <jarintasnin09@gmail.com>
2025-09-05 23:45:33 +0600

    Add Reconstruct Itinerary

--- Eulerian Circuit/Reconstruct Itinerary/ReconstructItinerary.cpp
+++ Eulerian Circuit/Reconstruct Itinerary/ReconstructItinerary.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, multiset<string>> graph;
 
+        for (auto& ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
+        }
+
+        vector<string> itinerary;
+        stack<string> dfs_stack;
+        dfs_stack.push("JFK");
+
+        while (!dfs_stack.empty()) {
+            string current = dfs_stack.top();
+
+            if (graph[current].empty()) {
+                itinerary.push_back(current);
+                dfs_stack.pop();
+            } else {
+                auto next = *(graph[current].begin());
+                graph[current].erase(graph[current].begin());
+                dfs_stack.push(next);
+            }
+        }
+
+        reverse(itinerary.begin(), itinerary.end());
+        return itinerary;
+    }
+};
