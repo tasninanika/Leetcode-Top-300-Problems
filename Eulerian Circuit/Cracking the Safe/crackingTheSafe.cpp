69f1ecb6ed9c59c1c40818fb1db4c674594fe99e
anikatasnin <jarintasnin09@gmail.com>
2025-09-06 14:42:05 +0600

    Add Cracking the Safe

--- Eulerian Circuit/Cracking the Safe/crackingTheSafe.cpp
+++ Eulerian Circuit/Cracking the Safe/crackingTheSafe.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    string crackSafe(int n, int k) {
+        string start = string(n - 1, '0');
+        unordered_set<string> visited;
+        string result;
 
+        dfs(start, visited, result, k, n);
+
+        return result + start;
+    }
+
+private:
+    void dfs(const string& node, unordered_set<string>& visited, string& result, int k, int n) {
+        for (int i = 0; i < k; ++i) {
+            string next = node + char('0' + i);
+            if (visited.find(next) == visited.end()) {
+                visited.insert(next);
+                dfs(next.substr(1), visited, result, k, n);
+                result.push_back('0' + i);
+            }
+        }
+    }
+};
