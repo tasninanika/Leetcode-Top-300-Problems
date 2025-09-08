50234df78e8523b6ff634e353f7ca822d6ad6b72
anikatasnin <jarintasnin09@gmail.com>
2025-09-08 23:26:31 +0600

    Add Min Cost Climbing Stairs

--- 1-D DP/Min Cost Climbing Stairs/MinCostClimbingStairs.cpp
+++ 1-D DP/Min Cost Climbing Stairs/MinCostClimbingStairs.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int minCostClimbingStairs(std::vector<int>& cost) {
+        int n = cost.size();
+        int prev1 = 0;
+        int prev2 = 0;
+        int current;
 
+        for (int i = 2; i <= n; ++i) {
+            current = std::min(prev1 + cost[i - 1], prev2 + cost[i - 2]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+};
