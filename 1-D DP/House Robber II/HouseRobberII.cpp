e88ea42a6d1cfe1cb7037f2c45f057dd35760e51
anikatasnin <jarintasnin09@gmail.com>
2025-09-10 23:36:55 +0600

    Add House Robber II

--- 1-D DP/House Robber II/HouseRobberII.cpp
+++ 1-D DP/House Robber II/HouseRobberII.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int rob_linear(std::vector<int>& nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        for (int i = start; i <= end; ++i) {
+            int temp = std::max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        return prev1;
+    }
 
+    int rob(std::vector<int>& nums) {
+        if (nums.empty()) return 0;
+        if (nums.size() == 1) return nums[0];
+
+        return std::max(rob_linear(nums, 0, nums.size() - 2), rob_linear(nums, 1, nums.size() - 1));
+    }
+};
