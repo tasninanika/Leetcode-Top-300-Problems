585994923e39694dba4962ce82430c02c7487fba
anikatasnin <jarintasnin09@gmail.com>
2025-09-12 10:26:59 +0600

    Add Target Sum

--- Knapsack DP/Target Sum/TargetSum.cpp
+++ Knapsack DP/Target Sum/TargetSum.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
 
+        if ((totalSum + target) % 2 != 0 || abs(target) > totalSum) {
+            return 0;
+        }
+
+        int subsetSum = (totalSum + target) / 2;
+
+        vector<int> dp(subsetSum + 1, 0);
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = subsetSum; j >= num; --j) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[subsetSum];
+    }
+};
