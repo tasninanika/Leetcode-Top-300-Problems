a13c2875a2c75f82a653bf2c6384c6321dbd582c
anikatasnin <jarintasnin09@gmail.com>
2025-09-13 16:21:10 +0600

    Add Last Stone Weight II

--- Knapsack DP/Last Stone Weight II/LastStoneWeightII.cpp
+++ Knapsack DP/Last Stone Weight II/LastStoneWeightII.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int totalSum = 0;
+        for (int stone : stones) {
+            totalSum += stone;
+        }
 
+        int target = totalSum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int stone : stones) {
+            for (int j = target; j >= stone; --j) {
+                dp[j] = dp[j] || dp[j - stone];
+            }
+        }
+
+        for (int s1 = target; s1 >= 0; --s1) {
+            if (dp[s1]) {
+                return totalSum - 2 * s1;
+            }
+        }
+
+        return 0;
+    }
+};
