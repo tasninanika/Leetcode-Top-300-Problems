142a6703c7b010eaa092ed744012a79d2123a7b4
anikatasnin <jarintasnin09@gmail.com>
2025-09-14 14:31:54 +0600

    Add Coin Change II

--- Unbounded Knapsack DP/Coin Change II/CoinChangeII.cpp
+++ Unbounded Knapsack DP/Coin Change II/CoinChangeII.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int dp[301][5001];
 
+    int coinChange(int amount, vector<int>& coins, int index) {
+        if (amount == 0) return 1;
+        if (amount < 0 || index >= coins.size()) return 0;
+        if (dp[index][amount] != -1) return dp[index][amount];
+
+        int include = coinChange(amount - coins[index], coins, index);
+        int exclude = coinChange(amount, coins, index + 1);
+
+        return dp[index][amount] = include + exclude;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        memset(dp, -1, sizeof(dp));
+        return coinChange(amount, coins, 0);
+    }
+};
