d18fa4a445b377f5924cf97181cf86cc5b04f9f3
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 14:57:53 +0600

    Add Number of Longest Increasing Subsequence

--- Longest Increasing Subsequence DP/Russian Doll Envelopes/RussianDollEnvelopes.cpp
+++ Longest Increasing Subsequence DP/Russian Doll Envelopes/RussianDollEnvelopes.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        if(envelopes.empty()) return 0;
 
+        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b){
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+        });
+
+        vector<int> heights;
+        for(const auto& envelope : envelopes)
+            heights.push_back(envelope[1]);
+
+        vector<int> lis;
+        for(int height : heights) {
+            auto it = lower_bound(lis.begin(), lis.end(), height);
+            if(it == lis.end()) {
+                lis.push_back(height);
+            } else {
+                *it = height;
+            }
+        }
+        return lis.size();
+    }
+};
