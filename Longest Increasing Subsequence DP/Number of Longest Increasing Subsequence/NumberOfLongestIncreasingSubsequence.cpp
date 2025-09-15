4d571e2192f6ee66a2294a3346214d0db217b82f
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 14:54:42 +0600

    Add Number of Longest Increasing Subsequence

--- Longest Increasing Subsequence DP/Number of Longest Increasing Subsequence/NumberOfLongestIncreasingSubsequence.cpp
+++ Longest Increasing Subsequence DP/Number of Longest Increasing Subsequence/NumberOfLongestIncreasingSubsequence.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
 
+        if (n == 0) return 0;
+
+        vector<int> dp(n, 1);
+        vector<int> count(n, 1);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+        }
+
+        int longest = *max_element(dp.begin(), dp.end());
+        int result = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (dp[i] == longest) {
+                result += count[i];
+            }
+        }
+
+        return result;
+    }
+};
