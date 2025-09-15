012c0a9e347d0ea8f31c38deaffbb1382779743b
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 14:51:46 +0600

    Add Longest Increasing Subsequence

--- Longest Increasing Subsequence DP/Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
+++ Longest Increasing Subsequence DP/Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) return 0;
 
+        vector<int> dp(nums.size(), 1);
+
+        for(size_t i = 1; i < nums.size(); ++i) {
+            for(size_t j = 0; j < i; ++j) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
