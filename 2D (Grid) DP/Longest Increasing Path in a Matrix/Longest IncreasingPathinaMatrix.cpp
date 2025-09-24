e4762759ed9ebf2a7231ed8e0e7708c26f8746ff
anikatasnin <jarintasnin09@gmail.com>
2025-09-24 08:42:16 +0600

    Add Longest Increasing Path in a Matrix

--- 2D (Grid) DP/Longest Increasing Path in a Matrix/Longest IncreasingPathinaMatrix.cpp
+++ 2D (Grid) DP/Longest Increasing Path in a Matrix/Longest IncreasingPathinaMatrix.cpp
@@ -1,3 +1,7 @@
 class Solution {
 public:
     int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    int dfs(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& memo) {
+        if (memo[i][j] != 0) return memo[i][j];
+
