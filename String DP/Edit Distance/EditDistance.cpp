c4b94437ceeb8bdf8fa5fba26e89c19de1fbe61b
anikatasnin <jarintasnin09@gmail.com>
2025-09-26 20:08:23 +0600

    Add Edit Distance

--- String DP/Edit Distance/EditDistance.cpp
+++ String DP/Edit Distance/EditDistance.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
 
+        for (int j = 0; j <= n; ++j) prev[j] = j;
+
+        for (int i = 1; i <= m; ++i) {
+            curr[0] = i;
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    curr[j] = prev[j - 1];
+                } else {
+                    curr[j] = 1 + min({prev[j],
+                                       curr[j - 1],
+                                       prev[j - 1]
+                                      });
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
