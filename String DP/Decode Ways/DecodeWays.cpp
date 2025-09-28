6a23c5f2ff3201ee04dc786de5dea770251dde5f
anikatasnin <jarintasnin09@gmail.com>
2025-09-28 19:07:37 +0600

    Add Decode Ways

--- String DP/Decode Ways/DecodeWays.cpp
+++ String DP/Decode Ways/DecodeWays.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0) return 0;
 
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0')
+              continue;
+
+            dp[i] = dp[i + 1];
+
+            if (i < n - 1) {
+                int twoDigit = stoi(s.substr(i, 2));
+                if (twoDigit >= 10 && twoDigit <= 26) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
