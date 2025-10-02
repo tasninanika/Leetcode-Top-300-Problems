1ec54c6580af55ba6a4d1583fca5c76f314b7669
anikatasnin <jarintasnin09@gmail.com>
2025-10-02 19:15:10 +0600

    Add Distinct Subsequences

--- String DP/Distinct Subsequences/DistinctSubsequences.cpp
+++ String DP/Distinct Subsequences/DistinctSubsequences.cpp
@@ -7,8 +7,10 @@ public:
 
 private:
     int countSubsequences(const string &s, const string &t, int i, int j, vector<vector<int>>& dp) {
-        if (j == t.size()) return 1;
-        if (i == s.size()) return 0;
+        if (j == t.size())
+            return 1;
+        if (i == s.size())
+            return 0;
 
         if (dp[i][j] != -1) return dp[i][j];
 
