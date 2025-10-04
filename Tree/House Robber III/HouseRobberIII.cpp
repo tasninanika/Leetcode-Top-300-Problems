3392e43b36c1b5fd7799f06254e37ec7688107a2
anikatasnin <jarintasnin09@gmail.com>
2025-10-04 13:44:27 +0600

    Add House Robber III

--- Tree/House Robber III/HouseRobberIII.cpp
+++ Tree/House Robber III/HouseRobberIII.cpp
@@ -1,1 +1,19 @@
+class Solution {
+    pair<int, int> robSub(TreeNode* node) {
+        if (!node) return {0, 0};
 
+        auto left = robSub(node->left);
+        auto right = robSub(node->right);
+
+        int robCurrent = node->val + left.second + right.second;
+        int skipCurrent = max(left.first, left.second) + max(right.first, right.second);
+
+        return {robCurrent, skipCurrent};
+    }
+
+public:
+    int rob(TreeNode* root) {
+        auto result = robSub(root);
+        return max(result.first, result.second);
+    }
+};
