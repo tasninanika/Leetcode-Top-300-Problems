f4e3cc06e7e83a96d1ffc865aacc95505cb80432
anikatasnin <jarintasnin09@gmail.com>
2025-10-06 18:45:19 +0600

    Add Number of Ways to Arrive at Destination

--- Tree/Number of Ways to Arrive at Destination/NumberOfWaysToArriveAtDestination.cpp
+++ Tree/Number of Ways to Arrive at Destination/NumberOfWaysToArriveAtDestination.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        const int MOD = 1e9 + 7;
+        vector<vector<pair<int, int>>> graph(n);
 
+        for (const auto& road : roads) {
+            int u = road[0], v = road[1], w = road[2];
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto [currentDist, u] = pq.top();
+            pq.pop();
+
+            if (currentDist > dist[u]) continue;
+
+            for (const auto& [v, weight] : graph[u]) {
+                long long newDist = currentDist + weight;
+                if (newDist < dist[v]) {
+                    dist[v] = newDist;
+                    ways[v] = ways[u];
+                    pq.push({newDist, v});
+                } else if (newDist == dist[v]) {
+                    ways[v] = (ways[v] + ways[u]) % MOD;
+                }
+            }
+        }
+
+        return ways[n - 1];
+    }
+};
