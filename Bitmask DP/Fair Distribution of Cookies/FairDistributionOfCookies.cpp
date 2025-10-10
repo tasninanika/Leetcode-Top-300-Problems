ba6e7751828ff2f5d0633cc1be789f8830562c57
anikatasnin <jarintasnin09@gmail.com>
2025-10-10 12:14:06 +0600

    Add Fair Distribution of Cookies

--- Bitmask DP/Fair Distribution of Cookies/FairDistributionOfCookies.cpp
+++ Bitmask DP/Fair Distribution of Cookies/FairDistributionOfCookies.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    void distribute(int index, vector<int>& cookies, vector<int>& distribution, int& minUnfairness) {
+        if (index == cookies.size()) {
+            int maxCookies = *max_element(distribution.begin(), distribution.end());
+            minUnfairness = min(minUnfairness, maxCookies);
+            return;
+        }
 
+        for (int i = 0; i < distribution.size(); ++i) {
+            distribution[i] += cookies[index];
+            distribute(index + 1, cookies, distribution, minUnfairness);
+            distribution[i] -= cookies[index];
+        }
+    }
+
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> distribution(k, 0);
+        int minUnfairness = INT_MAX;
+        distribute(0, cookies, distribution, minUnfairness);
+        return minUnfairness;
+    }
+};
