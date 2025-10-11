e327da11096de278a5304bf5d3a7fb7139f0a577
anikatasnin <jarintasnin09@gmail.com>
2025-10-11 15:17:56 +0600

    Add Shortest Path Visiting All Nodes

--- Bitmask DP/Shortest Path Visiting All Nodes/ShortestPathVisitingAllNodes.cpp
+++ Bitmask DP/Shortest Path Visiting All Nodes/ShortestPathVisitingAllNodes.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int fullMask = (1 << n) - 1;
+        vector<vector<bool>> vis(n, vector<bool>((1 << n), false));
+        queue<pair<int, int>> q;
+        for(int i = 0; i < n; ++i) {
+            q.push({i, 1 << i});
+            vis[i][1 << i] = true;
+        }
+        int level = 0;
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                auto [node, currMask] = q.front(); q.pop();
+                if(currMask == fullMask) return level;
+                for(auto &it : graph[node]) {
+                    int nextMask = currMask | (1 << it);
+                    if(!vis[it][nextMask]) {
+                        q.push({it, nextMask});
+                        vis[it][nextMask] = true;
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
