b21d5770e62b6e3c55b72fe20c6a5d4344232bba
anikatasnin <jarintasnin09@gmail.com>
2025-10-12 23:06:53 +0600

    added Count Numbers with Unique Digits

--- Digit DP/Count Numbers with Unique Digits/CountNumbersWithUniqueDigits.cpp
+++ Digit DP/Count Numbers with Unique Digits/CountNumbersWithUniqueDigits.cpp
@@ -3,3 +3,7 @@ public:
     int countNumbersWithUniqueDigits(int n) {
         if (n == 0) return 1;
         if (n == 1) return 10;
+
+        int result = 10;
+        int unique_digit_count = 9;
+        int available_digits = 9;
