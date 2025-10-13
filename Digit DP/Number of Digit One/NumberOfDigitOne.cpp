51d6a655ccfac5f8bb77c434a07054ccd72d4c51
anikatasnin <jarintasnin09@gmail.com>
2025-10-13 10:47:42 +0600

    Add Number of Digit One

--- Digit DP/Number of Digit One/NumberOfDigitOne.cpp
+++ Digit DP/Number of Digit One/NumberOfDigitOne.cpp
@@ -4,4 +4,28 @@ public:
         if (n <= 0)
             return 0;
 
+        long long count = 0;
+        long long factor = 1;
+
+        while (n / factor > 0) {
+            long long lower = n - (n / factor) * factor;
+            long long currDigit = (n / factor) % 10;
+            long long higher = n / (factor * 10);
+
+            if (currDigit == 0) {
+                count += higher * factor;
+            }
+            else if (currDigit == 1) {
+                count += higher * factor + (lower + 1);
+            }
+            else {
+                count += (higher + 1) * factor;
+            }
+
+            factor *= 10;
+        }
+
+        return count;
+    }
+};
 
