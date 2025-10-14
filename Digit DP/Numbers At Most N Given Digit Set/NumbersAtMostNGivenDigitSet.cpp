2709e990b0e2badaec27e6e2f5cf63a3287f5bf6
anikatasnin <jarintasnin09@gmail.com>
2025-10-14 10:25:03 +0600

    Add Numbers At Most N Given Digit Set

--- Digit DP/Numbers At Most N Given Digit Set/NumbersAtMostNGivenDigitSet.cpp
+++ Digit DP/Numbers At Most N Given Digit Set/NumbersAtMostNGivenDigitSet.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& D, int N) {
+        string NS = to_string(N);
+        int K = NS.size();
+        int dp[K + 1];
+        dp[K] = 1;
 
+        for (int i = K - 1; i >= 0; --i) {
+            int Si = NS[i] - '0';
+            dp[i] = 0;
+
+            for (auto &d : D) {
+                if (d[0] - '0' < Si) {
+                    dp[i] += pow(D.size(), K - i - 1);
+                } else if (d[0] - '0' == Si) {
+                    dp[i] += dp[i + 1];
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i < K; ++i) {
+            answer += pow(D.size(), i);
+        }
+
+        return answer + dp[0];
+    }
+};
