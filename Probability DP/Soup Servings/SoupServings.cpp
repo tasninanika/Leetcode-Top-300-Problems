38cbbb0b3b92ca2eb03635ecb1b61514a9178460
anikatasnin <jarintasnin09@gmail.com>
2025-10-16 18:12:46 +0600

    Add Soup Servings

--- Probability DP/Soup Servings/SoupServings.cpp
+++ Probability DP/Soup Servings/SoupServings.cpp
@@ -2,3 +2,8 @@ class Solution {
 public:
     unordered_map<int, unordered_map<int, double>> memo;
 
+    double serve(int A, int B) {
+        if (A <= 0 && B <= 0) return 0.5;
+        if (A <= 0) return 1.0;
+        if (B <= 0) return 0.0;
+
