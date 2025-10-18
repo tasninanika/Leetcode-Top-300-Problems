901a21343685cb5372f90b58011dec89767be1eb
anikatasnin <jarintasnin09@gmail.com>
2025-10-18 11:10:08 +0600

    Add Best Time to Buy and Sell Stock with Cooldown

--- State Machine DP/Best Time to Buy and Sell Stock with Cooldown/BestTimeToBuyAndSellStockWithCooldown.cpp
+++ State Machine DP/Best Time to Buy and Sell Stock with Cooldown/BestTimeToBuyAndSellStockWithCooldown.cpp
@@ -10,5 +10,11 @@ public:
         held[0] = -prices[0];
         sold[0] = 0;
         rest[0] = 0;
+
+        for (int i = 1; i < n; ++i) {
+            held[i] = max(held[i - 1], rest[i - 1] - prices[i]);
+            sold[i] = held[i - 1] + prices[i];
+            rest[i] = max(rest[i - 1], sold[i - 1]);
+        }
     }
 };
