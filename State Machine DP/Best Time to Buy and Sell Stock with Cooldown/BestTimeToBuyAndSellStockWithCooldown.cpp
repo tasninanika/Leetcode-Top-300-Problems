bbe2ecd4dde6a64d90405783b8e6746446529813
anikatasnin <jarintasnin09@gmail.com>
2025-10-18 11:09:44 +0600

    Add Best Time to Buy and Sell Stock with Cooldown

--- State Machine DP/Best Time to Buy and Sell Stock with Cooldown/BestTimeToBuyAndSellStockWithCooldown.cpp
+++ State Machine DP/Best Time to Buy and Sell Stock with Cooldown/BestTimeToBuyAndSellStockWithCooldown.cpp
@@ -6,5 +6,9 @@ public:
 
         int n = prices.size();
         vector<int> held(n, 0), sold(n, 0), rest(n, 0);
+
+        held[0] = -prices[0];
+        sold[0] = 0;
+        rest[0] = 0;
     }
 };
