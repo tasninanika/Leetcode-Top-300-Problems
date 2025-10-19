25f4eaa361d8ff43505599d6ed880f896d766583
anikatasnin <jarintasnin09@gmail.com>
2025-10-19 10:10:37 +0600

    Add Best Time to Buy and Sell Stock III

--- State Machine DP/Best Time to Buy and Sell Stock III/BestTimeToBuyAndSellStockIII.cpp
+++ State Machine DP/Best Time to Buy and Sell Stock III/BestTimeToBuyAndSellStockIII.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy1 = INT_MIN, sell1 = 0;
+        int buy2 = INT_MIN, sell2 = 0;
 
+        for (const auto& price : prices) {
+            buy1 = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
+            buy2 = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
+        }
+
+        return sell2;
+    }
+};
