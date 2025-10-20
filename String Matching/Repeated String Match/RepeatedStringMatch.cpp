35315b756878c27c71ce572bc708f8a6929fc155
anikatasnin <jarintasnin09@gmail.com>
2025-10-20 21:10:05 +0600

    Add Repeated String Match

--- String Matching/Repeated String Match/RepeatedStringMatch.cpp
+++ String Matching/Repeated String Match/RepeatedStringMatch.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int n = a.length(), m = b.length();
+        string A = a;
+
+        int count = 1;
+        while(A.size() < b.size()){
+            A += a;
+            count++;
+        }
+        if( A.find(b) != string::npos) return count;
+
+        A += a;
+        count++;
+        if( A.find(b) != string::npos) return count;
+
+        return -1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
