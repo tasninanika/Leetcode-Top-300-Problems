937fc6aa7e82af69f36b6f7cc69a6fb29c7e21b8
anikatasnin <jarintasnin09@gmail.com>
2025-10-21 11:06:20 +0600

    Add Shortest Palindrome

--- String Matching/Shortest Palindrome/ShortestPalindrome.cpp
+++ String Matching/Shortest Palindrome/ShortestPalindrome.cpp
@@ -1,1 +1,6 @@
-
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        string temp = s + "#" + rev_s;
