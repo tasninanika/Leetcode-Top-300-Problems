97cca13d434c21b4afe473084e1ddd612bf646bb
anikatasnin <jarintasnin09@gmail.com>
2025-10-21 11:06:53 +0600

    Add Shortest Palindrome

--- String Matching/Shortest Palindrome/ShortestPalindrome.cpp
+++ String Matching/Shortest Palindrome/ShortestPalindrome.cpp
@@ -4,3 +4,10 @@ public:
         string rev_s = s;
         reverse(rev_s.begin(), rev_s.end());
         string temp = s + "#" + rev_s;
+
+        vector<int> kmp(temp.size(), 0);
+        for (int i = 1; i < temp.size(); ++i) {
+            int j = kmp[i - 1];
+            while (j > 0 && temp[i] != temp[j]) {
+                j = kmp[j - 1];
+            }
