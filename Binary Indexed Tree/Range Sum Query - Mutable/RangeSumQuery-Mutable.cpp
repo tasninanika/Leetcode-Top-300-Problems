83d54b067ebcc681a651c96eebe401dd88048ebb
anikatasnin <jarintasnin09@gmail.com>
2025-10-22 23:39:21 +0600

    Add Range Sum Query - Mutable

--- Binary Indexed Tree/Range Sum Query - Mutable/RangeSumQuery-Mutable.cpp
+++ Binary Indexed Tree/Range Sum Query - Mutable/RangeSumQuery-Mutable.cpp
@@ -14,4 +14,43 @@ private:
         tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
     }
 
+    void updateTree(int treeIndex, int start, int end, int arrIndex, int val) {
+        if (start == end) {
+            tree[treeIndex] = val;
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        if (arrIndex <= mid) {
+            updateTree(2 * treeIndex + 1, start, mid, arrIndex, val);
+        } else {
+            updateTree(2 * treeIndex + 2, mid + 1, end, arrIndex, val);
+        }
+        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+    }
 
+    int queryTree(int treeIndex, int start, int end, int queryLeft, int queryRight) {
+        if (end < queryLeft || start > queryRight) return 0;
+        if (queryLeft <= start && end <= queryRight) return tree[treeIndex];
+        int mid = start + (end - start) / 2;
+        int leftSum = queryTree(2 * treeIndex + 1, start, mid, queryLeft, queryRight);
+        int rightSum = queryTree(2 * treeIndex + 2, mid + 1, end, queryLeft, queryRight);
+        return leftSum + rightSum;
+    }
+
+public:
+    NumArray(vector<int>& nums) {
+        if (!nums.empty()) {
+            n = nums.size();
+            tree.resize(4 * n);
+            buildTree(nums, 0, 0, n - 1);
+        }
+    }
+
+    void update(int index, int val) {
+        updateTree(0, 0, n - 1, index, val);
+    }
+
+    int sumRange(int left, int right) {
+        return queryTree(0, 0, n - 1, left, right);
+    }
+};
