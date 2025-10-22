62fefce8334279069f6775ed77b7149b74c27779
anikatasnin <jarintasnin09@gmail.com>
2025-10-22 23:39:04 +0600

    Add Range Sum Query - Mutable

--- Binary Indexed Tree/Range Sum Query - Mutable/RangeSumQuery-Mutable.cpp
+++ Binary Indexed Tree/Range Sum Query - Mutable/RangeSumQuery-Mutable.cpp
@@ -1,1 +1,17 @@
+class NumArray {
+private:
+    vector<int> tree;
+    int n;
+
+    void buildTree(const vector<int>& nums, int treeIndex, int start, int end) {
+        if (start == end) {
+            tree[treeIndex] = nums[start];
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        buildTree(nums, 2 * treeIndex + 1, start, mid);
+        buildTree(nums, 2 * treeIndex + 2, mid + 1, end);
+        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+    }
+
 
