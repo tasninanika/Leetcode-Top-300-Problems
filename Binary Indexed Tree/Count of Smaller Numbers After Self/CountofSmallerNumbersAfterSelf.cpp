bb891df25e9a7a8126c2b2808e60d7adfd0ab4ac
anikatasnin <jarintasnin09@gmail.com>
2025-10-23 15:00:19 +0600

    Add Count of Smaller Numbers After Self

--- Binary Indexed Tree/Count of Smaller Numbers After Self/CountofSmallerNumbersAfterSelf.cpp
+++ Binary Indexed Tree/Count of Smaller Numbers After Self/CountofSmallerNumbersAfterSelf.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int offset = 10000;
+        int size = 2 * offset + 1;
+        vector<int> BIT(size + 1, 0);
+        vector<int> result(nums.size(), 0);
 
+        auto update = [&](int index, int value) {
+            while (index < BIT.size()) {
+                BIT[index] += value;
+                index += (index & -index);
+            }
+        };
+
+        auto query = [&](int index) {
+            int sum = 0;
+            while (index > 0) {
+                sum += BIT[index];
+                index -= (index & -index);
+            }
+            return sum;
+        };
+
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int num = nums[i] + offset;
+            result[i] = query(num);
+            update(num + 1, 1);
+        }
+        return result;
+    }
+};
