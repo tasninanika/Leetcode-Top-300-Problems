b6f05365d4fbca3d722b3ccde1eb9d03103c0f06
anikatasnin <jarintasnin09@gmail.com>
2025-10-27 09:34:56 +0600

    Add Valid Square

--- Geometry/Valid Square/ValidSquare.cpp
+++ Geometry/Valid Square/ValidSquare.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-
+         vector<int> d = {
+            dist(p1,p2), dist(p1,p3), dist(p1,p4),
+            dist(p2,p3), dist(p2,p4),
+            dist(p3,p4)
+        };
     }
 };
