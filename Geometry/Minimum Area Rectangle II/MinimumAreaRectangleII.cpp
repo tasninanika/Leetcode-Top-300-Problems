fbb1dd88218c561f64dafe673d83dd8d0cece6cb
anikatasnin <jarintasnin09@gmail.com>
2025-10-28 09:06:23 +0600

    Add Minimum Area Rectangle II

--- Geometry/Minimum Area Rectangle II/MinimumAreaRectangleII.cpp
+++ Geometry/Minimum Area Rectangle II/MinimumAreaRectangleII.cpp
@@ -11,3 +11,7 @@ public:
         for(int i=0; i<points.size(); i++){
             for(int j=i+1; j<points.size(); j++){
                 for(int k=j+1; k<points.size(); k++){
+                    int dx1 = points[j][0]-points[i][0];
+                    int dx2 = points[k][0]-points[i][0];
+                    int dy1 = points[j][1]-points[i][1];
+                    int dy2 = points[k][1]-points[i][1];
