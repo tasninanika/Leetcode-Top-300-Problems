4a2c1c3456a05d5f64a26faeb04a5bf93419289b
anikatasnin <jarintasnin09@gmail.com>
2025-10-28 09:05:20 +0600

    Add Minimum Area Rectangle II

--- Geometry/Minimum Area Rectangle II/MinimumAreaRectangleII.cpp
+++ Geometry/Minimum Area Rectangle II/MinimumAreaRectangleII.cpp
@@ -1,2 +1,7 @@
 class Solution {
 public:
+     double minAreaFreeRect(vector<vector<int>>& points) {
+        unordered_map<int, unordered_set<int>> mp;
+        for(auto p: points){
+            mp[p[0]].insert(p[1]);
+        }
